3431a7451fde4ce61008282cfaf47e001acd97f1
John Corey <john@jcorey.org>
1999-10-29 14:55:22 +0000

    GPLify this.
    svn path=/trunk/kdebase/kioslave/; revision=32167

--- imap4/imap4.h
+++ imap4/imap4.h
@@ -1,7 +1,27 @@
 #ifndef _IMAP4_H
 #define _IMAP4_H "$Id$"
-
-#define NO_KIO_COMPATABILITY
+/**********************************************************************
+ *
+ *   imap4.h  - IMAP4rev1 KIOSlave
+ *   Copyright (C) 1999  John Corey
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *   Send comments and bug fixes to jcorey@fruity.ath.cx
+ *
+ *********************************************************************/
 
 #include <sys/types.h>
 #include <sys/time.h>
