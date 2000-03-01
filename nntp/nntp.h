429a099b65c405d323083b7d360885aefb16eda9
Christopher Molnar <molnar@kde.org>
2000-03-01 01:15:48 +0000

    CJM - Start of re-building kio_nntp for new KIO
    svn path=/trunk/kdebase/kioslave/; revision=42214

--- nntp/nntp.h
+++ nntp/nntp.h
@@ -1,9 +1,11 @@
 #ifndef _NNTP_H
 #define _NNTP_H "$Id$"
-#ifndef __kio_smb_h__
-#define __kio_smb_h__
 
 #include <qstring.h>
+#include <qcstring.h>
+#include <kurl.h>
+#include <stdio.h>
+#include <sys/types.h>
 #include <kio/global.h>
 #include <kio/slavebase.h>
 
@@ -19,18 +21,14 @@ public:
 	// Uses this function to get information in the url
 	virtual void openConnection(const QString& host, int ip, const QString& user, const QString& pass);
 	virtual void closeConnection();
-
+	virtual void special(const QByteArray &);
 
 
 protected:
 
-	void createUDSEntry( const QString & filename, const QString & path, KIO::UDSEntry & entry)
-	void createUDSEntry( const SMBdirent* dent, const QString & path, KIO::UDSEntry & entry) 	
-
-	NNTP *m_pNNTP;
-	
 	QString currentHost, currentIP, currentUser, currentPass;
 	QString buildFullLibURL(const QString &pathArg);
+	QString host, pass, user;
 };
 
 #endif
