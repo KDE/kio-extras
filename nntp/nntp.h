c1b011f5f1019ae2a4f2fbeb30ae32bc765d877c
Christopher Molnar <molnar@kde.org>
2000-03-03 17:42:26 +0000

    *** empty log message ***
    svn path=/trunk/kdebase/kioslave/; revision=42416

--- nntp/nntp.h
+++ nntp/nntp.h
@@ -1,33 +1,77 @@
+
 #ifndef _NNTP_H
+#define _NNTP_H
 #define _NNTP_H "$Id$"
 
-#include <qstring.h>
-#include <qcstring.h>
-#include <kurl.h>
-#include <stdio.h>
 #include <sys/types.h>
-#include <kio/global.h>
+#include <sys/time.h>
+
+#include <stdio.h>
+
+#include <qstring.h>
+
 #include <kio/slavebase.h>
-
-
 
 class NNTPProtocol : public KIO::SlaveBase
 {
-
 public:
-	NNTPProtocol( KIO::Connection *_conn = 0);
-	virtual ~NNTPProtocol();
+  NNTPProtocol (KIO::Connection *connection = 0 );
+  virtual ~NNTPProtocol();
 
-	// Uses this function to get information in the url
-	virtual void setHost(const QString& host, int ip, const QString& user, const QString& pass);
-	virtual void special(const QByteArray &);
+  virtual void openConnection( const QString& host, int port, const QString& user, const QString& pass );
+  virtual void closeConnection();
 
+  virtual void get( const QString& __url, const QString& query, bool reload );
+  virtual void stat( const QString & );
+  virtual void del( const QString &path, bool isfile);
+  virtual void listDir( const QString& path );
+  
+ protected:
 
-protected:
+  /**
+    * This returns the size of a message as a long integer.
+    * This is useful as an internal member, because the "other"
+    * getSize command will emit a signal, which would be harder
+    * to trap when doing something like listing a directory.
+    */
+  size_t realGetSize(unsigned int msg_num);
 
-	QString currentHost, currentIP, currentUser, currentPass;
-	QString buildFullLibURL(const QString &pathArg);
-	QString host, pass, user;
+  /**
+    *  Send a command to the server, and wait for the one-line-status
+    *  reply via getResponse.  Similar rules apply.  If no buffer is
+    *  specified, no data is passed back.
+    */
+  bool command (const char *buf, char *r_buf=0, unsigned int r_len=0);
+
+  /**
+    *  All NNTP commands will generate a response.  Each response will
+    *  either be prefixed with a "+OK " or a "-ERR ".  The getResponse 
+    *  fucntion will wait until there's data to be read, and then read in
+    *  the first line (the response), and copy the response sans +OK/-ERR
+    *  into a buffer (up to len bytes) if one was passed to it.  It will
+    *  return true if the response was affirmitave, or false otherwise.
+    */
+  bool getResponse (char *buf=0, unsigned int len=0);
+
+  /**
+    *  Attempt to properly shut down the NNTP connection by sending
+    *  "QUIT\r\n" before closing the socket.
+    */
+  void nntp_close ();
+
+  /**
+    * Attempt to initiate a NNTP connection via a TCP socket.  If no port
+    * is passed, port 110 is assumed, if no user || password is
+    * specified, the user is prompted for them.
+    */
+  bool nntp_open( KURL &_url );
+
+  int m_cmd, m_iSock;
+  unsigned short int m_iOldPort;
+  struct timeval m_tTimeout;
+  QString m_sOldServer, m_sOldPass, m_sOldUser;
+  FILE *fp;
+  QString urlPrefix;
 };
 
 #endif
