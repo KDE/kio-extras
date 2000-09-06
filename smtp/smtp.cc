1023254091cc797624d96ddd3688f48173d9207b
George Staikos <staikos@kde.org>
2000-09-06 05:46:36 +0000

    licence additions
    svn path=/trunk/kdebase/kioslave/; revision=62854

--- smtp/smtp.cc
+++ smtp/smtp.cc
@@ -1,3 +1,23 @@
+/**
+ * smtp.cpp
+ *
+ * Copyright (c) 2000 George Staikos <staikos@kde.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
@@ -37,10 +57,7 @@
 
 //
 //
-// By George Staikos <staikos@kde.org> 5/2000
 // Based partially on the pop3 ioslave.
-//
-// Released under the QPL.
 //
 
 using namespace KIO;
