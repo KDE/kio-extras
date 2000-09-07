16b1ee647e6804463d21c3f566ded17a10ca7a49
Alexander Neundorf <neundorf@kde.org>
2000-09-07 10:01:31 +0000

    changed my mail address in the copyright notice to @kde.org :-)
    Btw. why isn't the nfs ioslave listed in the web page dealing with the
    copyrights ?
    
    Bye
    Alex
    
    svn path=/trunk/kdebase/kioslave/; revision=63010

--- nfs/kio_nfs.h
+++ nfs/kio_nfs.h
@@ -1,5 +1,5 @@
-/*  This file is part of the KDE libraries
-    Copyright (C) 2000 Alexander Neundorf <alexander.neundorf@rz.tu-ilmenau.de>
+/*  This file is part of the KDE project
+    Copyright (C) 2000 Alexander Neundorf <neundorf@kde.org>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
