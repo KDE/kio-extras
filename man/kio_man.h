b38597c50dfe5dc7533fa1581190efc37f76b008
Alexander Neundorf <neundorf@kde.org>
2000-11-04 11:32:28 +0000

    copyright to mhk
    svn path=/trunk/kdebase/kioslave/; revision=69893

--- man/kio_man.h
+++ man/kio_man.h
@@ -1,5 +1,6 @@
 /*  This file is part of the KDE libraries
-    Copyright (C) 2000 Stephan Kulow <coolo@kde.org>
+    Copyright (c) 2000 Matthias Hoelzer-Kluepfel <mhk@caldera.de>
+ 
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
