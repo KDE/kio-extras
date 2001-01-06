0853b319f5f046d1ecae001285a238a05f8f2e03
Alex Zepeda <zipzippy@sonic.net>
2001-01-06 09:25:29 +0000

    Remove a bunch of unnecesary stuff.
    svn path=/trunk/kdebase/kioslave/; revision=76591

--- smtp/base64.h
+++
@@ -1,8 +0,0 @@
-#ifndef _BASE64_H
-#define _BASE64_H "$Id$"
-
-QString base64_decode_string(const char *string, unsigned int len);
-QString base64_encode_string( const char *buf, unsigned int len );
-QString create_auth_plain(const char *username, const char *pass);
-
-#endif
--- smtp/cram_auth.h
+++
@@ -1,6 +0,0 @@
-#ifndef CRAM_AUTH_H
-#define CRAM_AUTH_H "$Id$"
-
-QString generate_cram_auth(const char *user, const char *pass, const char *shared);
-
-#endif
