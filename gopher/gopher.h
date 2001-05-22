ee57874b66b1c9338d4e836fa2950fe74b948bed
Alex Zepeda <zipzippy@sonic.net>
2001-05-22 08:23:47 +0000

    Warnings and license stuff.
    svn path=/trunk/kdebase/kioslave/; revision=98221

--- gopher/gopher.h
+++ gopher/gopher.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1999,2000 Alex Zepeda
+ * Copyright (c) 1999,2000,2001 Alex Zepeda <jazepeda@pacbell.net>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -10,6 +10,18 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
+ * 3. Redistributions of source code or in binary form must consent to
+ *    future terms and conditions as set forth by the founding author(s).
+ *    The founding author is defined as the creator of following code, or
+ *    lacking a clearly defined creator, the founding author is defined as
+ *    the first person to claim copyright to, and contribute significantly
+ *    to the following code.
+ * 4. The following code may be used without explicit consent in any
+ *    product provided the previous three conditions are met, and that
+ *    the following source code be made available at no cost to consumers
+ *    of mentioned product and the founding author as defined above upon
+ *    request.  This condition may at any time be waived by means of 
+ *    explicit written consent from the founding author.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -23,7 +35,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id$
+ * $Id$
  */
 
 #ifndef _GOPHER_H
@@ -60,37 +72,36 @@ enum GopherType
 class GopherProtocol : public KIO::TCPSlaveBase
 {
 public:
-public:
-  GopherProtocol (const QCString &pool, const QCString &app );
-  virtual ~GopherProtocol();
+	GopherProtocol (const QCString &pool, const QCString &app );
+	virtual ~GopherProtocol ();
 
-  virtual void get( const KURL&);
-  virtual void stat( const KURL& url );
-  //virtual void del( const KURL& url, bool isfile);
-  virtual void listDir( const KURL& url );
+	virtual void get (const KURL &);
+	virtual void stat (const KURL &url);
+	//virtual void del (const KURL& url, bool isfile);
+	virtual void listDir (const KURL& url);
 
-  void setHost(const QString &host, int port, const QString &user, const QString &pass);
+	void setHost (const QString &host, int port, const QString &user, const QString &pass);
 
- protected:
+protected:
 
-  bool readRawData(const QString &_url, const char *mimetype=0);
+	bool readRawData(const QString &_url, const char *mimetype=0);
 
-  /**
-    *  Attempt to properly shut down the Gopher connection.
-    */
-  void gopher_close ();
+	/**
+	  *  Attempt to properly shut down the Gopher connection.
+	*/
+	void gopher_close ();
 
-  /**
-    * Attempt to initiate a Gopher connection via a TCP socket.  If no port
-    * is passed, port 70 is assumed.
-    */
-  bool gopher_open (const KURL &_url);
+	/**
+	  * Attempt to initiate a Gopher connection via a TCP socket.  If no
+	  * port is passed, port 70 is assumed.
+	*/
+	bool gopher_open (const KURL &_url);
 
-  QString m_sServer, m_sUser, m_sPass;
-  int m_cmd;
-  struct timeval m_tTimeout;
-  GopherType current_type;
-  static const char *abouttext;
+	QString m_sServer, m_sUser, m_sPass;
+	int m_cmd;
+	struct timeval m_tTimeout;
+	GopherType current_type;
+	static const char *abouttext;
 };
 
 #endif
