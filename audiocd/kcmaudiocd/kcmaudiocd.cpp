d7dc5b1ed8148759c226ed4d4aade428e924a2db
Carsten Pfeiffer <pfeiffer@kde.org>
2001-07-21 11:32:34 +0000

    sorry for breaking the translation, but it's Konqueror, not Konqeror
    svn path=/trunk/kdebase/kioslave/; revision=107059

--- audiocd/kcmaudiocd/kcmaudiocd.cpp
+++ audiocd/kcmaudiocd/kcmaudiocd.cpp
@@ -592,7 +592,7 @@ QString KAudiocdModule::quickHelp() const
 {
     return i18n("<h1>Audio-CD Slave</h1> The Audio-CD Slave enables you to easily"
                         " rip wav, MP3 or ogg vorbis files from your CD-ROM or DVD drive."
-                        " The slave is invoked by typing <i>\"audiocd:/\"</i> in Konqeror's location"
+                        " The slave is invoked by typing <i>\"audiocd:/\"</i> in Konqueror's location"
                         " bar. In this module, you can configure all aspects of the slave like"
                         " encoding, CDDB lookup and device settings. Note that MP3 and ogg"
                         " vorbis encoding are only available if the KDE was built with a recent"
