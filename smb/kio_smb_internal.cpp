d4c5d236a4d5654d0fc84f199aef231859157bfe
Frank Schwanz <schwanz@fh-brandenburg.de>
2002-01-08 16:25:34 +0000

    complete checking of URL's smb://[[domain;]user[:password]@]server[:port][/share[/path[/file]]] smb:/[[domain;]user[:password]@][group/[server[/share[/path[/file]]]]]
    svn path=/trunk/kdebase/kioslave/; revision=130596

--- smb/kio_smb_internal.cpp
+++ smb/kio_smb_internal.cpp
@@ -69,17 +69,49 @@ SMBUrl& SMBUrl::append(const char* filedir)
     return *this;
 }
 
+
+//-----------------------------------------------------------------------
+void SMBUrl::setUser(const QString &userinfo)
+//-----------------------------------------------------------------------
+{
+
+      // extract domain
+      if (userinfo.contains(';')) {
+          m_userdomain = userinfo.left(userinfo.find(';'));
+          m_user =  userinfo.right(userinfo.length()-userinfo.find(';')-1);
+      }
+      else
+          m_user =  userinfo;
+}
+
 //-----------------------------------------------------------------------
 void SMBUrl::fromKioUrl(const KURL& kurl)
 //-----------------------------------------------------------------------
 {
     m_type     = SMBURLTYPE_UNKNOWN;
     m_kio_url  = kurl.prettyURL();
+    // remove userinfo from m_kio_url
+    if (m_kio_url.contains('@')) {
+      int pos_at = m_kio_url.find('@');
+      int pos_slash = m_kio_url.findRev('/', pos_at);
+      int pos_dp = m_kio_url.findRev(':', pos_at);
+      // set password
+      if (pos_dp>5)
+          m_password = m_kio_url.mid(pos_dp+1,pos_at-pos_dp-1);
+      // set userinfo
+      if (pos_dp>5)
+          setUser(m_kio_url.mid(pos_slash+1, pos_dp-pos_slash-1));
+      else
+          setUser(m_kio_url.mid(pos_slash+1, pos_at-pos_slash-1));
+      m_kio_url.remove(pos_slash+1, pos_at-pos_slash);
+      // on "smb://" the password is stored int pass()
+      if (!kurl.pass().isEmpty())
+        m_password = kurl.pass();
+    }
     m_smbc_url = m_kio_url;
-    m_smbc_url = m_smbc_url.insert(4,'/');
-
     m_workgroup_index = m_kio_url.find('/') + 1;
     m_workgroup_len = m_kio_url.find('/',m_workgroup_index);
+
     if(m_workgroup_len < 0)
     {
         m_workgroup_len = 0; //m_kio_url.length();
@@ -93,6 +125,19 @@ void SMBUrl::fromKioUrl(const KURL& kurl)
         if( m_smbc_url[ end_index ] == '/' )
             m_smbc_url.remove( end_index, 1 );
     }
+		// remove Workgroup if smb:/workgroup/host/share
+		if ( ((t == SMBURLTYPE_WORKGROUP_OR_SERVER) ||
+         (t == SMBURLTYPE_SHARE_OR_PATH)) &&
+        !m_smbc_url.contains("smb://") &&
+        (m_smbc_url.find('/', 5)>0)    &&
+        (m_smbc_url.find('/', 5)<(int)m_smbc_url.length()-1)) {
+          int pos_slash_host = m_kio_url.find('/', 5);
+          int pos_slash_wg = m_smbc_url.findRev('/',  pos_slash_host-1);
+          m_smbc_url.remove( pos_slash_wg+1, pos_slash_host-pos_slash_wg);
+        }
+		if (!m_smbc_url.contains("smb://"))
+	    m_smbc_url = m_smbc_url.insert(4,'/');
+
 }
 
 //-----------------------------------------------------------------------   
@@ -100,8 +145,9 @@ QCString SMBUrl::toSmbcUrl() const
 // Return a URL that is suitable for libsmbclient
 //----------------------------------------------------------------------- 
 {
-    kdDebug(KIO_SMB) << "toSmbcURL, returning: " << QString(m_smbc_url.local8Bit()) << endl;
+		kdDebug(KIO_SMB) << "toSmbcURL, returning: " << QString(m_smbc_url.local8Bit()) << endl;
     return m_smbc_url.local8Bit();
+
 }
 
 //----------------------------------------------------------------------- 
@@ -144,16 +190,23 @@ SMBUrlType SMBUrl::getType()
         return m_type;
     }
 
-    // skip the "smb:/"
-    pos1 = pos1 + 5;
+    // skip the "smb://"
+	  if (m_kio_url.contains("smb://"))
+	    pos1 = 6;
+		else  // skip "smb:/"
+			pos1 = 5;
 
     // Check for the workgroup
     pos2 = m_kio_url.find('/',pos1);
-    if((pos2 == -1) || (pos2 == (m_kio_url.length()-1)))
-    {
+    if (!m_kio_url.contains("smb://") && (pos2!=-1))  // means smb:/workgroup/host
+      pos2 = m_kio_url.find('/',pos2+1);
+
+    if ((pos2 == -1) || (pos2 == (int)(m_kio_url.length()-1)))
+    {   // smb://host/
         m_type = SMBURLTYPE_WORKGROUP_OR_SERVER;
         return m_type;
     }
+
 
     // Check for the path if we get this far
     m_type = SMBURLTYPE_SHARE_OR_PATH;
@@ -187,4 +240,23 @@ QString SMBUrl::getServerShareDir() const
     return m_kio_url.right(m_kio_url.length() - (m_workgroup_index + m_workgroup_len)).local8Bit();
 }
 
+//-----------------------------------------------------------------------
+QString SMBUrl::getUser() const
+//-----------------------------------------------------------------------
+{
+    return m_user;
+}
+
+//-----------------------------------------------------------------------
+QString SMBUrl::getPassword() const
+//-----------------------------------------------------------------------
+{
+    return m_password;
+}
+//-----------------------------------------------------------------------
+QString SMBUrl::getUserDomain() const
+//-----------------------------------------------------------------------
+{
+    return m_userdomain;
+}
 
