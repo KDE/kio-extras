2b67869235bb38d6619327b0c4dce7bb17842e70
Stephan Kulow <coolo@kde.org>
2002-11-15 09:32:18 +0000

    don't hardcode the icon theme -    QCString cmd = cmds.arg(m_perl).arg(m_infoScript).arg(locate("data", "kio_info/kde-info2html.conf")).arg(KGlobal::dirs()->findResourceDir("icon", "crystalsvg/22x22/actions/up.png")).arg(m_page).arg(m_node).latin1(); +    QString path = KGlobal::iconLoader()->iconPath("up", KIcon::Toolbar, true); + +    for (int i = 0; i < 4; ++i) { +        int revindex = path.findRev('/'); +        path = path.left(revindex); +    } +    QCString cmd = cmds.arg(m_perl).arg(m_infoScript).arg(locate("data", "kio_info/kde-info2html.conf")).arg(path).arg(m_page).arg(m_node).latin1();
    svn path=/trunk/kdebase/kioslave/; revision=189070

--- info/info.cc
+++ info/info.cc
@@ -10,6 +10,7 @@
 #include <kdebug.h>
 #include <kprocess.h>
 #include <kstandarddirs.h>
+#include <kiconloader.h>
 #include <kinstance.h>
 
 #include "info.h"
@@ -26,10 +27,10 @@ InfoProtocol::InfoProtocol( const QCString &pool, const QCString &app )
     m_infoScript = locate( "data", "kio_info/kde-info2html" );
 
     m_perl = KGlobal::dirs()->findExe( "perl" );
-    
+
     if( m_infoScript.isEmpty() )
 	kdError( 7108 ) << "Critical error: Cannot locate 'kde-info2html' for HTML-conversion" << endl;
-    
+
     kdDebug( 7108 ) << "InfoProtocol::InfoProtocol - done" << endl;
 }
 
@@ -58,14 +59,20 @@ void InfoProtocol::get( const KURL& url )
     decodeURL( url );
 
     QString cmds("%1 %2 %3 \"%4\" \"%5\" \"%6\"");
-    QCString cmd = cmds.arg(m_perl).arg(m_infoScript).arg(locate("data", "kio_info/kde-info2html.conf")).arg(KGlobal::dirs()->findResourceDir("icon", "crystalsvg/22x22/actions/up.png")).arg(m_page).arg(m_node).latin1();
+    QString path = KGlobal::iconLoader()->iconPath("up", KIcon::Toolbar, true);
+
+    for (int i = 0; i < 4; ++i) {
+        int revindex = path.findRev('/');
+        path = path.left(revindex);
+    }
+    QCString cmd = cmds.arg(m_perl).arg(m_infoScript).arg(locate("data", "kio_info/kde-info2html.conf")).arg(path).arg(m_page).arg(m_node).latin1();
     kdDebug( 7108 ) << "cmd: " << (const char *)cmd << endl;
-    
+
     FILE *fd = popen( cmd.data(), "r" );
-    
+
     char buffer[ 4090 ];
     QByteArray array;
-    
+
     while ( !feof( fd ) )
     {
       int n = fread( buffer, 1, 2048, fd );
@@ -79,11 +86,11 @@ void InfoProtocol::get( const KURL& url )
       data( array );
       array.resetRawData( buffer, n );
     }
-    
+
     pclose( fd );
 
     finished();
-    
+
     kdDebug( 7108 ) << "InfoProtocol::get - done" << endl;
 }
 
@@ -108,7 +115,7 @@ void InfoProtocol::decodeURL( const KURL &url )
      * else test for valid dir in url.host()+url.path() because
      *  of "info://usr/local/info/infopage/Top" made from the
      *  "info2html" program.
-     * If there is no possibility to get a valid dir, assume the url to 
+     * If there is no possibility to get a valid dir, assume the url to
      *  be a valid page description.
      */
 
@@ -142,7 +149,7 @@ void InfoProtocol::decodeURL( const KURL &url )
       oldPos = slashPos;
       slashPos = dirstr.find( "/", oldPos+1 );
       if (-1 == slashPos) {
-	// no more '/' found, 
+	// no more '/' found,
 	// the whole string is a valid path ?
 	break;
       }
@@ -198,7 +205,7 @@ QCString InfoProtocol::errorMessage()
 
 // A minimalistic stat with only the file type
 // This seems to be enough for konqueror
-void InfoProtocol::stat( const KURL &url )
+void InfoProtocol::stat( const KURL & )
 {
 	UDSEntry uds_entry;
 	UDSAtom  uds_atom;
@@ -217,47 +224,47 @@ void InfoProtocol::stat( const KURL &url )
 /*void InfoProtocol::listDir( const KURL &url )
 {
     kdDebug( 7108 ) << "InfoProtocol::listDir" << endl;
- 
+
     if ( !url.directory(true,true).isEmpty()
          && url.directory(true,true) != QString("/") )
     {
         error( KIO::ERR_CANNOT_ENTER_DIRECTORY, url.path() );
         return;
     }
- 
+
     // Match info nodes in the 'dir' file
     // "* infopage:" at the start of a line
     QRegExp regex( "^\\*  *[^: ][^:]*:", false );
- 
+
     QFile f( "/usr/info/dir" );
- 
+
     if ( f.open(IO_ReadOnly) ) {
         QTextStream t( &f );
         QString s;
- 
+
         int start, len;
- 
+
         UDSEntryList uds_entry_list;
         UDSEntry     uds_entry;
         UDSAtom      uds_atom;
- 
+
         uds_atom.m_uds = KIO::UDS_NAME; // we only do names...
         uds_entry.append( uds_atom );
- 
+
         while ( !t.eof() ) {
             s = t.readLine();
- 
+
             start = regex.match( s, 0, &len );
- 
+
             if ( start != -1 ) {
             // Found "* infonode:", add "infonode" to matches
- 
+
                 int pos = 1;
                 while ( pos < len && s[pos] == ' ')
                     pos++;
- 
+
                 QString name = s.mid( pos, (len-pos-1) ).lower();
- 
+
                 if ( !name.isEmpty() ) {
                     uds_entry[0].m_str = name;
                     uds_entry_list.append( uds_entry );
@@ -265,7 +272,7 @@ void InfoProtocol::stat( const KURL &url )
             }
         }
         f.close();
- 
+
         listEntries( uds_entry_list );
         finished();
     }
