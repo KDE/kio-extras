0dc0f0f96dd48f4a6eb674755aa06461100dffba
David Pashley <david@parguild.co.uk>
2003-05-25 12:22:46 +0000

    Add some doxygen comments
    svn path=/trunk/kdebase/kioslave/; revision=227752

--- imap4/imapcommand.h
+++ imap4/imapcommand.h
@@ -26,24 +26,111 @@
 #include <qstringlist.h>
 #include <qstring.h>
 
+/** @class imapCommand
+ *  @author Svenn Carstens
+ *  @date 2000
+ *  @todo fix the documentation
+ */
+
 class imapCommand
 {
 public:
+
+   /** 
+    * @brief Constructor 
+    */
   imapCommand ();
+   /** 
+    * @fn imapCommand (const QString & command, const QString & parameter);
+    * @brief Constructor 
+    * @param command Imap command
+    * @param parameter Parameters to the command
+    * @return none
+    */
   imapCommand (const QString & command, const QString & parameter);
+  /**
+   * @fn bool isComplete ();
+   * @brief is it complete?
+   * @return whether the command is completed
+   */
   bool isComplete ();
+  /** 
+   * @fn const QString & result ();
+   * @brief get the result
+   * @return The result
+   */
   const QString & result ();
+  /** 
+   * @fn const QString & resultInfo ();
+   * @brief get information about the result
+   * @return Information about the result
+   */
   const QString & resultInfo ();
+  /** 
+   * @fn const QString & parameter ();
+   * @brief get the parameter
+   * @return the parameter
+   */
   const QString & parameter ();
+  /** 
+   * @fn const QString & command ();
+   * @brief get the command
+   * @return the command
+   */
   const QString & command ();
+  /** 
+   * @fn const QString & id ();
+   * @brief get the id
+   * @return the id
+   */
   const QString & id ();
 
+  /** 
+   * @fn void setId (const QString &);
+   * @brief set the id
+   * @param id the id used by the command
+   * @return none
+   */
   void setId (const QString &);
+  /** 
+   * @fn void setComplete ();
+   * @brief set the completed state
+   * @return none
+   */
   void setComplete ();
+  /** 
+   * @fn void setResult (const QString &);
+   * @brief set the completed state
+   * @param result the command result
+   * @return none
+   */
   void setResult (const QString &);
+  /** 
+   * @fn void setResultInfo (const QString &);
+   * @brief set the completed state
+   * @param result the command result information
+   * @return none
+   */
   void setResultInfo (const QString &);
+  /** 
+   * @fn void setCommand (const QString &);
+   * @brief set the command
+   * @param result the imap command
+   * @return none
+   */
   void setCommand (const QString &);
+  /** 
+   * @fn void setParameter (const QString &);
+   * @brief set the command parameter(s)
+   * @param result the comand parameter(s)
+   * @return none
+   */
   void setParameter (const QString &);
+  /** 
+   * @fn const QString getStr ();
+   * @brief get a string
+   * @return a string
+   */
   const QString getStr ();
 
   static imapCommand *clientNoop ();
