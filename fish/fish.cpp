041114f7e32b74ac1528b58d0cf678bef688ba59
Alex Zepeda <zipzippy@sonic.net>
2003-07-07 08:10:19 +0000

    CCMAIL: 60083-done@bugs.kde.org
    svn path=/trunk/kdebase/kioslave/; revision=235891

--- fish/fish.cpp
+++ fish/fish.cpp
@@ -301,7 +301,7 @@ void fishProtocol::openConnection() {
 
 static int open_pty_pair(int fd[2])
 {
-#if defined(HAVE_TERMIOS_H) && defined(HAVE_GRANTPT)
+#if defined(HAVE_TERMIOS_H) && defined(HAVE_GRANTPT) && !defined(HAVE_OPENPTY)
 /** with kind regards to The GNU C Library
 Reference Manual for Version 2.2.x of the GNU C Library */
     int master, slave;
