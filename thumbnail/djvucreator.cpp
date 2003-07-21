7eae7511c7c04b08f2115f2a69aabd44abb88df2
Leon Bottou <leon@bottou.org>
2003-07-21 20:40:08 +0000

    Code is closer to gscreator.
    svn path=/trunk/kdebase/kioslave/; revision=238416

--- thumbnail/djvucreator.cpp
+++ thumbnail/djvucreator.cpp
@@ -74,56 +74,63 @@ bool DjVuCreator::create(const QString &path, int width, int height, QImage &img
   argv[6] = 0;
   
   pid_t pid = fork(); 
-  if (pid == 0) 
+  if (pid < 0)
+    {
+      return false;
+    }
+
+  if (pid > 0)
     {
       close(output[0]);
       dup2(output[1], STDOUT_FILENO);	  
       execvp(argv[0], const_cast<char *const *>(argv));
       exit(1);
     }
-  else 
+  else if (pid == 0)
     {
       close(output[1]);
-      
-      if (pid != -1)
-        {
-          int offset = 0;
-          while (!ok) {
-            fd_set fds;
-            FD_ZERO(&fds);
-            FD_SET(output[0], &fds);
-            struct timeval tv;
-            tv.tv_sec = 20;
-            tv.tv_usec = 0;
-            if (select(output[0] + 1, &fds, 0, 0, &tv) <= 0) 
-              break; // error or timeout
-            if (FD_ISSET(output[0], &fds)) {
-              int count = read(output[0], data.data() + offset, 1024);
-              if (count == -1)
-                break;
-              if (count) // prepare for next block
-                {
-                  offset += count;
-                  data.resize(offset + 1024);
-                }
-              else // got all data
-                {
-                  data.resize(offset);
-                  ok = true;
-                }
+      int offset = 0;
+      while (!ok) {
+        fd_set fds;
+        FD_ZERO(&fds);
+        FD_SET(output[0], &fds);
+        struct timeval tv;
+        tv.tv_sec = 20;
+        tv.tv_usec = 0;
+        if (select(output[0] + 1, &fds, 0, 0, &tv) <= 0) 
+          break; // error or timeout
+        if (FD_ISSET(output[0], &fds)) {
+          int count = read(output[0], data.data() + offset, 1024);
+          if (count == -1)
+            break;
+          if (count) // prepare for next block
+            {
+              offset += count;
+              data.resize(offset + 1024);
             }
-          }
-          if (!ok) // error or timeout, gs probably didn't exit yet
-            kill(pid, SIGTERM);
-          int status;
-          if (waitpid(pid, &status, 0) != pid || (status != 0  && status != 256) )
-            ok = false;
+          else // got all data
+            {
+              data.resize(offset);
+              ok = true;
+            }
         }
+      }
+      if (!ok)
+        kill(pid, SIGTERM);
+      int status;
+      if (waitpid(pid, &status, 0) != pid || (status != 0  && status != 256) )
+        ok = false;
     }
-  close(output[1]);
+  else
+    {
+      close(output[1]);
+    }
+  
+  close(output[0]);
   int l = img.loadFromData( data );
   return ok && l;
 }
+
 
 ThumbCreator::Flags DjVuCreator::flags() const
 {
