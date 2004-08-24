8a9c45470a72c28c35f1ddf86d8528c86d57f650
Kevin Ottens <ervin@kde.org>
2004-08-24 01:01:30 +0000

    New kfile-plugin showing disk size and usage.
    CCMAIL: 58688-done@bugs.kde.org
    
    svn path=/trunk/kdebase/kioslave/; revision=340303

---
+++ devices/kfile-plugin/kfiledeviceplugin.cpp
@@ -0,0 +1,119 @@
+#include "kfiledeviceplugin.h"
+#include <kgenericfactory.h>
+#include <dcopclient.h>
+#include <qdatastream.h>
+#include <kdiskfreesp.h>
+#include <kapplication.h>
+#include <unistd.h>
+#include <kdebug.h>
+
+typedef KGenericFactory<KFileDevicePlugin> KFileDevicePluginFactory;
+K_EXPORT_COMPONENT_FACTORY(kfile_device, KFileDevicePluginFactory("kfile_device"))
+
+KFileDevicePlugin::KFileDevicePlugin(QObject *parent, const char *name,
+		                     const QStringList& args)
+	: KFilePlugin(parent, name, args)
+{
+	addMimeType( "kdedevice/cdrom_mounted" );
+	addMimeType( "kdedevice/cdwriter_mounted" );
+	addMimeType( "kdedevice/dvd_mounted" );
+	addMimeType( "kdedevice/floppy5_mounted" );
+	addMimeType( "kdedevice/floppy_mounted" );
+	addMimeType( "kdedevice/hdd_mounted" );
+	addMimeType( "kdedevice/zip_mounted" );
+}
+
+bool KFileDevicePlugin::readInfo(KFileMetaInfo &info, uint /*what*/)
+{
+	QString mount_point = askMountPoint(info);
+	
+	KDiskFreeSp *df = new KDiskFreeSp();
+	
+	m_done = false;
+	m_total = 0;
+	m_used = 0;
+	m_free = 0;
+	connect(df, SIGNAL( done() ), this, SLOT( slotDfDone() ));
+	connect(df, SIGNAL( foundMountPoint(const QString &,
+	                                     unsigned long,
+	                                     unsigned long,
+			                     unsigned long) ),
+	        this, SLOT( slotFoundMountPoint(const QString &,
+		                                unsigned long,
+						unsigned long,
+						unsigned long)) );
+	
+	df->readDF(mount_point);
+	
+	while(!m_done)
+	{
+		usleep(1000);
+		qApp->processEvents();
+	}
+
+	KFileMetaInfoGroup group = appendGroup(info, "deviceInfo");
+	           
+	appendItem(group, "free", (long long unsigned)m_free);
+	appendItem(group, "used", (long long unsigned)m_used);
+	appendItem(group, "total", (long long unsigned)m_total);
+	
+	return true;
+}
+
+void KFileDevicePlugin::slotFoundMountPoint(const QString &/*mountPoint*/,
+                                            unsigned long total, unsigned long used,
+                                            unsigned long free)
+{
+	m_free = free;
+	m_used = used;
+	m_total = total;
+}
+
+void KFileDevicePlugin::slotDfDone()
+{
+	m_done = true;
+}
+
+QString KFileDevicePlugin::askMountPoint(KFileMetaInfo &info)
+{
+	QByteArray data;
+	QByteArray param;
+	QCString retType;
+	QStringList retVal;
+	QDataStream streamout(param,IO_WriteOnly);
+	streamout<<info.url().fileName();
+
+	DCOPClient *client = kapp->dcopClient();
+
+	if(client->call("kded", "mountwatcher", "basicDeviceInfo(QString)",
+	                param,retType,data,false )
+	  )
+	{
+		QDataStream streamin(data,IO_ReadOnly);
+		streamin>>retVal;
+
+		return KURL(retVal[2]).path();
+	}
+
+	return QString::null;
+}
+
+void KFileDevicePlugin::addMimeType(const char *mimeType)
+{
+	KFileMimeTypeInfo *info = addMimeTypeInfo( mimeType );
+
+	KFileMimeTypeInfo::GroupInfo *group
+		= addGroupInfo(info, "deviceInfo", i18n("Device Information"));
+
+	KFileMimeTypeInfo::ItemInfo *item
+		= addItemInfo(group, "free", i18n("Free"), QVariant::Int);
+	setUnit(item, KFileMimeTypeInfo::KiloBytes);
+
+	item = addItemInfo(group, "used", i18n("Used"), QVariant::Int);
+	setUnit(item, KFileMimeTypeInfo::KiloBytes);
+
+	item = addItemInfo(group, "total", i18n("Total"), QVariant::Int);
+	setUnit(item, KFileMimeTypeInfo::KiloBytes);
+}
+
+#include "kfiledeviceplugin.moc"
---
+++ devices/kfile-plugin/kfiledeviceplugin.h
@@ -0,0 +1,32 @@
+#ifndef __KFILE_DEVICE_H__
+#define __KFILE_DEVICE_H__
+
+#include <kfilemetainfo.h>
+#include <kurl.h>
+
+class KFileDevicePlugin : public KFilePlugin
+{
+	Q_OBJECT
+public:
+	KFileDevicePlugin(QObject *parent, const char *name,
+	                  const QStringList &args);
+
+	bool readInfo(KFileMetaInfo &info, uint what = KFileMetaInfo::Fastest);
+
+private:
+	void addMimeType(const char *mimeType);
+	QString askMountPoint(KFileMetaInfo &info);
+
+	unsigned long m_total;
+	unsigned long m_used;
+	unsigned long m_free;
+	bool m_done;
+
+private slots:
+	void slotFoundMountPoint(const QString &mountPoint,
+	                         unsigned long total, unsigned long used,
+	                         unsigned long available);
+	void slotDfDone();
+};
+
+#endif
