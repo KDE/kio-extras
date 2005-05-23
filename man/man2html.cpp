06bad7ace2610a46504760be0a3923deaa76801c
Nicolas Goutte <nicolasg@snafu.de>
2005-05-23 06:57:15 +0000

    A few cleanups: - use Qt's qstr* functions where possible - remove include files when possible (especially if compiling for KIO). - use one of the config.h defines when possible
    svn path=/trunk/KDE/kdebase/kioslave/; revision=417241

--- man/man2html.cpp
+++ man/man2html.cpp
@@ -105,25 +105,35 @@
 **    time to look through all the available manpages.)
 */
 
+#ifdef SIMPLE_MAN2HTML
+  // We suppose that we run on a standard Linux
+# define HAVE_STRING_H 1
+# define HAVE_UNISTD_H 1
+#else
+# include <config.h>
+#endif
 
-#include <stdio.h>
-#include <iostream>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <dirent.h>
 #include <ctype.h>
-#include <sys/types.h>
-#include <time.h>
-#include <sys/time.h>
-#include <errno.h>
+
+#ifdef HAVE_UNISTD_H
+# include <unistd.h>
+#endif
+
+#ifdef HAVE_STRING_H
+# include <string.h>
+#endif
 
 #include <qvaluestack.h>
 #include <qstring.h>
 #include <qptrlist.h>
 
-#ifndef SIMPLE_MAN2HTML
+#ifdef SIMPLE_MAN2HTML
+# include <stdlib.h>
+# include <iostream>
+# include <stdio.h>
+# include <dirent.h>
+# include <sys/stat.h>
+#else
 # include <qtextcodec.h>
 # include <kdebug.h>
 #endif
@@ -183,8 +193,8 @@ static char *stralloc(int len)
 // ### TODO: replace by either qstrncpy or strlcpy
 static char *strmaxcpy(char *to, const char *from, int n)
 {				/* Assumes space for n plus a null */
-  int len = strlen(from);
-  strncpy(to, from, n);
+  int len = qstrlen(from);
+  qstrncpy(to, from, n);
   to[(len <= n) ? len : n] = '\0';
   return to;
 }
@@ -192,9 +202,9 @@ static char *strmaxcpy(char *to, const char *from, int n)
 #if 0 // unused
 static char *strmaxcat(char *to, char *from, int n)
 {				/* Assumes space for n plus a null */
-  int to_len = strlen(to);
+  int to_len = qstrlen(to);
   if (to_len < n) {
-    int from_len = strlen(from);
+    int from_len = qstrlen(from);
     int cp = (to_len + from_len <= n) ? from_len : n - to_len;
     strncpy(to + to_len, from, cp);
     to[to_len + cp] = '\0';
@@ -600,7 +610,7 @@ static const char *expand_char(int nr)
   charb[2]='\0';
   if (charb[0] == '<') {	/* Fix up <= */
     charb[4] = charb[1];
-    strncpy(charb, "&lt;", 4);
+    qstrncpy(charb, "&lt;", 4);
     charb[5] = '\0';
   }
   curpos+=2;
@@ -633,6 +643,7 @@ static const char *includedirs[] = {
     "/usr/ccs",
     "/usr/X11R6/include",
     "/usr/openwin/include",
+    "/usr/include/g++",
     0
 };
 
@@ -755,8 +766,8 @@ static void add_links(char *c)
 		}
 		if (isalnum(*h)) {
 		    char t,sec, *e;
-		    QString subsec;
-		    QString fstr(f);
+                    QString subsec; // ### TODO avoid using QString, as we do not know the encoding
+                    QString fstr(f); // ### TODO avoid using QString, as we do not know the encoding
 		    e=h+1;
 		    sec=f[1];
 		    subsec=f[2];
@@ -998,7 +1009,7 @@ static const char *change_to_size(int nr)
   if (nr) {
     int l;
     strcat(sizebuf, "<FONT SIZE=\"");
-    l=strlen(sizebuf);
+    l=qstrlen(sizebuf);
     if (nr>0) sizebuf[l++]='+'; else sizebuf[l++]='-',nr=-nr;
     sizebuf[l++]=nr+'0';
     sizebuf[l++]='"';
@@ -1070,7 +1081,7 @@ static char *scan_escape(char *c)
 	if (argument) {
 	    c++;
 	    i=(*c -'1');
-	    if (i < 0 || (*argument && i > (int)strlen(*argument)) || !(h=argument[i])) h="";
+	    if (i < 0 || (*argument && i > (int)qstrlen(*argument)) || !(h=argument[i])) h="";
 	}
 	break;
     case 'z':
@@ -1489,7 +1500,7 @@ static char *scan_table(char *c)
 	/* scan table options */
 	while (c<h) {
 	    while (isspace(*c)) c++;
-	    for (i=0; tableopt[i] && strncmp(tableopt[i],c,tableoptl[i]);i++);
+	    for (i=0; tableopt[i] && qstrncmp(tableopt[i],c,tableoptl[i]);i++);
 	    c=c+tableoptl[i];
 	    switch (i) {
 	    case 0: center=1; break;
@@ -1764,20 +1775,20 @@ static char *scan_expression(char *c, int *result)
 	}
 	c++;
 	h=c;
-	while (*c!= sep && (!tcmp || strncmp(c,tcmp,4))) c++;
+	while (*c!= sep && (!tcmp || qstrncmp(c,tcmp,4))) c++;
 	*c='\n';
 	scan_troff(h, 1, &st1);
 	*c=sep;
 	if (tcmp) c=c+3;
 	c++;
 	h=c;
-	while (*c!=sep && (!tcmp || strncmp(c,tcmp,4))) c++;
+	while (*c!=sep && (!tcmp || qstrncmp(c,tcmp,4))) c++;
 	*c='\n';
 	scan_troff(h,1,&st2);
 	*c=sep;
 	if (!st1 && !st2) value=1;
 	else if (!st1 || !st2) value=0;
-	else value=(!strcmp(st1, st2));
+	else value=(!qstrcmp(st1, st2));
 	delete [] st1;
         delete [] st2;
 	if (tcmp) c=c+3;
@@ -2010,7 +2021,7 @@ static const char *lookup_abbrev(char *c)
     int i=0;
 
     if (!c) return "";
-    while (abbrev_list[i] && strcmp(c,abbrev_list[i])) i=i+2;
+    while (abbrev_list[i] && qstrcmp(c,abbrev_list[i])) i=i+2;
     if (abbrev_list[i]) return abbrev_list[i+1];
     else return c;
 }
@@ -2324,7 +2335,7 @@ static int get_request(char *req, int len)
         "Tn", "nN", "%A", "%D", "%N", "%O", "%P", "%Q", "%V", "%B", "%J", "%R",
         "%T", "An", "Aq", "Bq", "Qq", "UR", "UE", "UN", 0 };
     int r = 0;
-    while (requests[r] && strncmp(req, requests[r], len)) r++;
+    while (requests[r] && qstrncmp(req, requests[r], len)) r++;
     return requests[r] ? r : REQ_UNKNOWN;
 }
 
@@ -2389,7 +2400,7 @@ static char *scan_request(char *c)
         i=V(c[0],c[1]); // ### TODO still needed?
         /* search macro database of self-defined macros */
         owndef = defdef;
-        while (owndef && strncmp(c, owndef->name, strlen(owndef->name)))
+        while (owndef && qstrncmp(c, owndef->name, qstrlen(owndef->name)))
             owndef=owndef->next;
         if (owndef)
         {
@@ -2416,7 +2427,7 @@ static char *scan_request(char *c)
                 delete [] h;
             }
             for (i=words;i<20; i++) wordlist[i]=NULL;
-            deflen = strlen(owndef->st);
+            deflen = qstrlen(owndef->st);
             for (i=0; (owndef->st[deflen+2+i]=owndef->st[i]); i++);
             oldargument=argument;
             argument=wordlist;
@@ -2476,7 +2487,7 @@ static char *scan_request(char *c)
                     while (*c && *c!='\n') c++;
                     c++;
                     h=c;
-                    while (*c && strncmp(c,".di",3)) while (*c && *c++!='\n');
+                    while (*c && qstrncmp(c,".di",3)) while (*c && *c++!='\n');
                     *c='\0';
                     de=strdef;
                     while (de && de->nr !=i) de=de->next;
@@ -2613,7 +2624,7 @@ static char *scan_request(char *c)
                         {
                             char *line=NULL;
                             c=scan_troff(c,1, &line);
-                            if (line && strncmp(line, "<BR>", 4))
+                            if (line && qstrncmp(line, "<BR>", 4))
                             {
                                 out_html(line);
                                 out_html("<BR>\n");
@@ -2751,7 +2762,7 @@ static char *scan_request(char *c)
                         while (*c && *c!='\n') c++,i++;
                     }
                     c++;
-                    while (*c && strncmp(c,endwith,i)) while (*c++!='\n');
+                    while (*c && qstrncmp(c,endwith,i)) while (*c++!='\n');
                     while (*c && *c++!='\n');
                     break;
                 }
@@ -3302,7 +3313,7 @@ static char *scan_request(char *c)
                         out_html("\n<H3>");
                     else
                         out_html("\n<H2>");
-                    mandoc_synopsis = strncmp(c, "SYNOPSIS", 8) == 0;
+                    mandoc_synopsis = qstrncmp(c, "SYNOPSIS", 8) == 0;
                     c = mandoc_command ? scan_troff_mandoc(c,1,NULL) : scan_troff(c,1,NULL);
                     if (mode)
                         out_html("</H3>\n");
@@ -3350,8 +3361,8 @@ static char *scan_request(char *c)
                             {
                                 if (wordlist[i][0] == '\007')
                                     wordlist[i]++;
-                                if (wordlist[i][strlen(wordlist[i])-1] == '\007')
-                                    wordlist[i][strlen(wordlist[i])-1] = 0;
+                                if (wordlist[i][qstrlen(wordlist[i])-1] == '\007')
+                                    wordlist[i][qstrlen(wordlist[i])-1] = 0;
                             }
                             output_possible=true;
                             out_html( DOCTYPE"<HTML>\n<HEAD>\n");
@@ -3437,7 +3448,7 @@ static char *scan_request(char *c)
                     out_html(change_to_font('I'));
                     if (words>1) wordlist[1][-1]='\0';
                     const char *c2=lookup_abbrev(wordlist[0]);
-                    curpos+=strlen(c2);
+                    curpos+=qstrlen(c2);
                     out_html(c2);
                     out_html(change_to_font('R'));
                     if (words>1)
@@ -3544,12 +3555,12 @@ static char *scan_request(char *c)
                     }
                     c = next_line;
                     sl=c;
-                    while (*c && strncmp(c,endmacro, strlen(endmacro))) c=skip_till_newline(c);
+                    while (*c && qstrncmp(c,endmacro, qstrlen(endmacro))) c=skip_till_newline(c);
 
                     de=defdef;
-                    while (de && strncmp(name, de->name, strlen(de->name)))
+                    while (de && qstrncmp(name, de->name, qstrlen(de->name)))
                         de=de->next;
-                    if (mode && de) olen=strlen(de->st);
+                    if (mode && de) olen=qstrlen(de->st);
                     j=olen+c-sl;
                     h = stralloc(j*2+4);
                     if (h)
@@ -3653,7 +3664,7 @@ static char *scan_request(char *c)
                 case REQ_It: // mdoc(7) "list ITem"
                 {
                     c=c+j;
-                    if (strncmp(c, "Xo", 2) == 0 && isspace(*(c+2)))
+                    if (qstrncmp(c, "Xo", 2) == 0 && isspace(*(c+2)))
                     c = skip_till_newline(c);
                     if (dl_set[itemdepth] & BL_DESC_LIST)
                     {
@@ -4314,7 +4325,7 @@ static char *scan_request(char *c)
                          * I don't want to miss anything out of the text.
                          */
                         char buf[4];
-                        strncpy(buf,c,2);
+                        qstrncpy(buf,c,2);
                         buf[2] = ' ';
                         buf[3] = '\0';
                         out_html(buf);	/* Print the command (it might just be text). */
@@ -4370,7 +4381,7 @@ static char *scan_troff(char *c, int san, char **result)
     if (result) {
 	if (*result) {
 	    buffer=*result;
-	    buffpos=strlen(buffer);
+	    buffpos=qstrlen(buffer);
 	    buffmax=buffpos;
 	} else {
             buffer = stralloc(LARGE_STR_MAX);
@@ -4581,9 +4592,9 @@ void scan_man_page(const char *man_page)
     section = 0;
 
     output_possible = false;
-    int strLength = strlen(man_page);
+    int strLength = qstrlen(man_page);
     char *buf = new char[strLength + 2];
-    strcpy(buf+1, man_page);
+    qstrcpy(buf+1, man_page);
     buf[0] = '\n';
 
     scan_troff(buf+1,0,NULL);
