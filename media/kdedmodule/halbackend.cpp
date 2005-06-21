092efd40ca94650a5eacff3dce83f2b5e7c33150
Jérôme Lodewyck <jerome.lodewyck@normalesup.org>
2005-06-21 18:03:00 +0000

    Updated copyright information
    svn path=/trunk/KDE/kdebase/kioslave/; revision=427741

--- media/kdedmodule/halbackend.cpp
+++ media/kdedmodule/halbackend.cpp
@@ -1,5 +1,5 @@
 /* This file is part of the KDE Project
-   Copyright (c) 2004 J��e Lodewyck <lodewyck@clipper.ens.fr>
+   Copyright (c) 2004 - 2005 Jérôme Lodewyck <lodewyck@clipper.ens.fr>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
