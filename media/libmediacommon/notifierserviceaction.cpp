25b73106246573281e1e0cb7634449bf96bf20c3
Jean-Remy Falleri <jr.falleri@laposte.net>
2005-07-27 08:38:11 +0000

    Added trailing slash to konqueror/servicemenus in locateLocal to have servicemenus created if it does not exists.
    svn path=/trunk/KDE/kdebase/kioslave/; revision=439141

--- media/libmediacommon/notifierserviceaction.cpp
+++ media/libmediacommon/notifierserviceaction.cpp
@@ -101,7 +101,7 @@ void NotifierServiceAction::updateFilePath()
 	QString action_name = m_service.m_strName;
 	action_name.replace(   " ", "_" );
 	
-	QDir actions_dir( locateLocal( "data", "konqueror/servicemenus", true ) );
+	QDir actions_dir( locateLocal( "data", "konqueror/servicemenus/", true ) );
 
 	QString filename = actions_dir.absFilePath( action_name + ".desktop" );
 	
