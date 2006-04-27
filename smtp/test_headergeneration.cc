4407c0b32634990eb6dca920a92fecf6da3f8e54
Dirk Mueller <mueller@kde.org>
2006-04-27 14:47:26 +0000

    compile++
    svn path=/trunk/KDE/kdebase/kioslave/; revision=534658

--- smtp/test_headergeneration.cc
+++ smtp/test_headergeneration.cc
@@ -57,7 +57,7 @@ int main( int , char ** ) {
     "\n";
 
   KioSMTP::Request request;
-  QCString result;
+  QByteArray result;
 
   request.setEmitHeaders( true );
   request.setFromAddress( "mutz@kde.org" );
