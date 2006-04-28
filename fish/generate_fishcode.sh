b35519a4f1164877d4cb09150d61da98b92bbceb
Alexander Neundorf <neundorf@kde.org>
2006-04-28 21:08:07 +0000

    -compile fish under UNIX, this probably doesn't work under FreeBSD (and OS X ?) yet, will work as soon as we rely on cmake >= 2.4.0
    Alex
    
    
    svn path=/trunk/KDE/kdebase/kioslave/; revision=535218

---
+++ fish/generate_fishcode.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# $1 is fish.pl
+# $2 is md5sum
+# $3 is the output file
+# $4 are the parameters for cut 
+
+SUM=`$2 $1 | cut -d ' ' $4 `
+echo '#define CHECKSUM "'$SUM'"' > $3
+echo 'static const char *fishCode(' >> $3
+sed -e 's/\\/\\\\/g;s/"/\\"/g;s/^[ 	]*/"/;/^"# /d;s/[ 	]*$/\\n"/;/^"\\n"$/d;s/{CHECKSUM}/'$SUM'/;' $1 >> $3 
+echo ');' >> $3
+
