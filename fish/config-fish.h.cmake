512930537b8dd685e38eaea7ae0cec7672942246
Laurent Montel <montel@kde.org>
2006-10-17 16:03:08 +0000

    Move check here
    svn path=/trunk/KDE/kdebase/runtime/; revision=596467

---
+++ fish/config-fish.h.cmake
@@ -0,0 +1,2 @@
+/* Define to 1 if you have the `isastream' function. */
+#define HAVE_ISASTREAM 1
--- fish/fish.cpp
+++ fish/fish.cpp
@@ -25,7 +25,7 @@
 */
 
 #include "config.h"
-
+#include "config-fish.h"
 #include <QFile>
 #include <q3socket.h>
 #include <QDateTime>
