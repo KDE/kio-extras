8bd739b62ac6449da19a472de8a2227cf3e1fc91
David Faure <faure@kde.org>
2007-05-04 13:51:35 +0000

    Major config.h cleanup, moved much stuff to config-X11.h and to config-fish.h
    svn path=/trunk/KDE/kdebase/runtime/; revision=661060

--- fish/config-fish.h.cmake
+++ fish/config-fish.h.cmake
@@ -1,2 +1,27 @@
 /* Define to 1 if you have the `isastream' function. */
 #define HAVE_ISASTREAM 1
+
+/* Define to 1 if you have the <termios.h> header file. */
+#cmakedefine HAVE_TERMIOS_H 1
+
+/* Define to 1 if you have the <util.h> header file. */
+#cmakedefine HAVE_UTIL_H 1
+
+/* Define to 1 if you have the <sys/ioctl.h> header file. */
+#cmakedefine HAVE_SYS_IOCTL_H 1
+
+/* Define to 1 if you have the <pty.h> header file. */
+#cmakedefine HAVE_PTY_H 1
+
+/* Define to 1 if you have the <libutil.h> header file. */
+#cmakedefine HAVE_LIBUTIL_H 1
+
+/* Define to 1 if you have the `getpt' function. */
+#cmakedefine HAVE_GETPT 1
+
+/* Define to 1 if you have the `grantpt' function. */
+#cmakedefine HAVE_GRANTPT 1
+
+/* Defines whether we can use the openpty() function */
+#cmakedefine HAVE_OPENPTY 1
+
