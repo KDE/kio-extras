355640c975e6e74041a4d46353decc05d2c37262
David Faure <faure@kde.org>
2007-06-27 12:45:29 +0000

    nothing system-specific in there
    svn path=/trunk/KDE/kdebase/runtime/; revision=680928

--- kurifilter-plugins/localdomain/localdomainurifilter.cpp
+++ kurifilter-plugins/localdomain/localdomainurifilter.cpp
@@ -18,8 +18,6 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-#include <config-runtime.h>
-
 #include "localdomainurifilter.h"
 
 #include <k3process.h>
