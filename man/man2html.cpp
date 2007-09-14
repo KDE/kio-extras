3c5f80a9ac92f7b566020fac4eb28f299bb682fb
Hasso Tepper <hasso@kde.org>
2007-09-14 09:18:35 +0000

    Sections for BSD systems according to mdoc(7).
    svn path=/trunk/KDE/kdebase/runtime/; revision=712343

--- man/man2html.cpp
+++ man/man2html.cpp
@@ -2667,6 +2667,16 @@ static const char *section_list[] = {
     "9S", "DDI and DKI Data Structures",
     "9s", "DDI and DKI Data Structures",
     "L", "Local Commands",
+#elif defined(__NetBSD__) || defined(__OpenBSD__) || defined(__FreeBSD__) || defined(__DragonFly__)
+    "1", "General Commands",
+    "2", "System Calls",
+    "3", "Library Functions",
+    "4", "Kernel Interfaces",
+    "5", "File Formats",
+    "6", "Games",
+    "7", "Miscellaneous Information",
+    "8", "System Manager's Manuals",
+    "9", "Kernel Developer's Manuals",
 #else
     // Other OS
     "1", "User Commands ",
