042fad1ad2ed6ce7d855d8acb64f9e276d5c3d6e
Ingo Klöcker <kloecker@kde.org>
2007-12-30 20:19:02 +0000

    Quote $1 because it might contain spaces (or other characters requiring quoting). This fixes compilation for source directories containing spaces.
    svn path=/trunk/KDE/kdebase/runtime/; revision=754826

--- fish/generate_fishcode.sh
+++ fish/generate_fishcode.sh
@@ -5,9 +5,9 @@
 # $3 is the output file
 # $4 are the parameters for cut 
 
-SUM=`$2 $1 | cut -d ' ' $4 `
+SUM=`$2 "$1" | cut -d ' ' $4 `
 echo '#define CHECKSUM "'$SUM'"' > $3
 echo 'static const char *fishCode(' >> $3
-sed -e 's/\\/\\\\/g;s/"/\\"/g;s/^[ 	]*/"/;/^"# /d;s/[ 	]*$/\\n"/;/^"\\n"$/d;s/{CHECKSUM}/'$SUM'/;' $1 >> $3 
+sed -e 's/\\/\\\\/g;s/"/\\"/g;s/^[ 	]*/"/;/^"# /d;s/[ 	]*$/\\n"/;/^"\\n"$/d;s/{CHECKSUM}/'$SUM'/;' "$1" >> $3 
 echo ');' >> $3
 
