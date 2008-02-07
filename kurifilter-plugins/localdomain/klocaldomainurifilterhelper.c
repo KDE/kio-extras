2c9995a9d31ce4c63a9d06111c831f6dac8ea1d8
Luboš Luňák <l.lunak@kde.org>
2008-02-07 15:49:52 +0000

    Extend the given local name to the same fully qualified name, even if the name is an alias (bnc#359563).
    svn path=/trunk/KDE/kdebase/runtime/; revision=772035

--- kurifilter-plugins/localdomain/klocaldomainurifilterhelper.c
+++ kurifilter-plugins/localdomain/klocaldomainurifilterhelper.c
@@ -26,6 +26,7 @@
 
 #include <netdb.h>
 #include <stdio.h>
+#include <string.h>
 
 int main( int argc, char* argv[] )
 {
@@ -36,7 +37,24 @@ int main( int argc, char* argv[] )
 
     ent = gethostbyname( argv[ 1 ] );
     if (ent)
-      fputs( ent->h_name, stdout );
+    {
+        int i;
+        int found = 0;
+        /* try to find the same fully qualified name first */
+        for( i = 0;
+             ent->h_aliases[ i ] != NULL;
+             ++i )
+        {
+            if( strncmp( argv[ 1 ], ent->h_aliases[ i ], strlen( argv[ 1 ] )) == 0 )
+            {
+                found = 1;
+                fputs( ent->h_aliases[ i ], stdout );
+                break;
+            }
+        }
+        if( !found )
+            fputs( ent->h_name, stdout );
+    }
 
     return (ent != NULL || h_errno == NO_ADDRESS) ? 0 : 1;
 }
