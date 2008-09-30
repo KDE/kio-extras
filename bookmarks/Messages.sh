690516d3422e79e414f3b749a48d382f344b1db0
Laurent Montel <montel@kde.org>
2008-09-30 07:46:20 +0000

    Clean it
    svn path=/trunk/KDE/kdebase/runtime/; revision=866142

--- bookmarks/Messages.sh
+++ bookmarks/Messages.sh
@@ -1,3 +1,2 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find -iname \*.cpp -o -iname \*.h` -o $podir/kio_bookmarks.pot
+$XGETTEXT *.cpp -o $podir/kio_bookmarks.pot
