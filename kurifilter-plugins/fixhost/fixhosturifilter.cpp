9e333c298b2ae66cc210511b996efd10682ba865
Dawit Alemayehu <adawit@kde.org>
2010-11-16 10:19:44 +0000

    Revert my previous commit that attempted to workaround the issue of file names being interpreted as hosts because it causes more problems than it solves...
    svn path=/trunk/KDE/kdebase/runtime/; revision=1197631

--- kurifilter-plugins/fixhost/fixhosturifilter.cpp
+++ kurifilter-plugins/fixhost/fixhosturifilter.cpp
@@ -80,15 +80,6 @@ bool FixHostUriFilter::filterUri( KUriFilterData& data ) const
                     return true;
                 }
             }
-            // Undo kshorturifilter's deficiency of filtering file names
-            // such as "fixhosturifilter.cpp" as a valid urls. We do it
-            // here because the dns lookup failure tells us it is not a url.
-            if (url.port() == -1 && !data.typedString().startsWith(protocol) &&
-                data.uriType() != KUriFilterData::Unknown) {
-                kDebug(7023) << "Reverting previous filtering...";
-                setFilteredUri(data, KUrl());
-                setUriType(data, KUriFilterData::Unknown);
-            }
         }
     }
 
