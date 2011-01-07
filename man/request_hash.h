1ba11ffce8103a4327daef725c87c0ac78decd96
Martin Koller <kollix@aon.at>
2011-01-07 20:55:19 +0000

    add LGPL license header
    svn path=/trunk/KDE/kdebase/runtime/; revision=1212663

--- man/request_hash.h
+++ man/request_hash.h
@@ -1,9 +1,24 @@
 #ifndef RequestHash_H
 #define RequestHash_H
 
-// This file is part of the KDE libraries
-// Copyright (C) 2011 Martin Koller <kollix@aon.at>
+/* This file is part of the KDE libraries
+   Copyright (C) 2011 Martin Koller <kollix@aon.at>
 
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
 
 // Some known missing requests from mdoc(7):
 // - start or end of quotings
