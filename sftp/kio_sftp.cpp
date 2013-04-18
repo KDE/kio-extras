2b0211e6bb2c49474db63d3f8bcf0e9214a306b9
Dan Vrátil <dvratil@redhat.com>
2013-04-18 09:49:53 +0200

    Don't format port number in a kio_sftp message
    REVIEW: 110046

--- sftp/kio_sftp.cpp
+++ sftp/kio_sftp.cpp
@@ -602,7 +602,7 @@ void sftpProtocol::openConnection() {
 
   kDebug(KIO_SFTP_DB) << "username=" << mUsername << ", host=" << mHost << ", port=" << effectivePort;
 
-  infoMessage(i18n("Opening SFTP connection to host %1:%2", mHost, effectivePort));
+  infoMessage(i18n("Opening SFTP connection to host %1:<numid>%2</numid>", mHost, effectivePort));
 
   if (mHost.isEmpty()) {
     kDebug(KIO_SFTP_DB) << "openConnection(): Need hostname...";
