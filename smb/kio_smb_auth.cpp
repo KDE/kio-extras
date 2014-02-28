58337fd61bb6163f7cff1f2ab9029fb10b3a338e
Aurélien Gâteau <agateau@kde.org>
2014-02-28 15:21:11 +0100

    Add missing klocalizedstring.h includes
    They are inherited from slavebase.h for now, but that should change soon

--- smb/kio_smb_auth.cpp
+++ smb/kio_smb_auth.cpp
@@ -34,6 +34,7 @@
 
 #include <kconfig.h>
 #include <kconfiggroup.h>
+#include <klocalizedstring.h>
 #include <stdlib.h>
 
 // call for libsmbclient
--- smb/kio_smb_mount.cpp
+++ smb/kio_smb_mount.cpp
@@ -23,6 +23,7 @@
 #include <unistd.h>
 #include <QByteArray>
 #include <QDir>
+#include <KLocalizedString>
 #include <KProcess>
 #include <kshell.h>
 
