951e2f2d49a8b1a3c025ba311d78070791a84923
Méven Car <meven29@gmail.com>
2020-05-04 08:32:21 +0200

    filenamesearch: Implement stat to display metainfo
    Summary:
    filenamesearch ioslave needed a stat implementation to be rendered properly in dolphin information panel.
    
    BUG: 420354
    FIXED-IN: 20.08
    
    Test Plan: Open filenamsearch:/ in dolphin and search something
    
    Reviewers: #dolphin, #frameworks, ngraham, elvisangelaccio
    
    Reviewed By: #dolphin, elvisangelaccio
    
    Subscribers: kde-frameworks-devel, kfm-devel
    
    Tags: #dolphin, #frameworks
    
    Differential Revision: https://phabricator.kde.org/D29197

--- filenamesearch/kio_filenamesearch.cpp
+++ filenamesearch/kio_filenamesearch.cpp
@@ -30,6 +30,7 @@
 #include <QUrl>
 #include <QUrlQuery>
 #include <QDBusInterface>
+#include <KLocalizedString>
 
 Q_LOGGING_CATEGORY(KIO_FILENAMESEARCH, "kio_filenamesearch")
 
@@ -42,6 +43,28 @@ FileNameSearchProtocol::FileNameSearchProtocol(const QByteArray &pool, const QBy
 
 FileNameSearchProtocol::~FileNameSearchProtocol()
 {
+}
+
+void FileNameSearchProtocol::stat(const QUrl& url)
+{
+    KIO::UDSEntry uds;
+    uds.reserve(9);
+    uds.fastInsert(KIO::UDSEntry::UDS_ACCESS, 0700);
+    uds.fastInsert(KIO::UDSEntry::UDS_FILE_TYPE, S_IFDIR);
+    uds.fastInsert(KIO::UDSEntry::UDS_MIME_TYPE, QStringLiteral("inode/directory"));
+    uds.fastInsert(KIO::UDSEntry::UDS_ICON_OVERLAY_NAMES, QStringLiteral("baloo"));
+    uds.fastInsert(KIO::UDSEntry::UDS_DISPLAY_TYPE, i18n("Search Folder"));
+    uds.fastInsert(KIO::UDSEntry::UDS_URL, url.url());
+
+    QUrlQuery query(url);
+    QString title = query.queryItemValue(QStringLiteral("title"), QUrl::FullyDecoded);
+    if (!title.isEmpty()) {
+        uds.fastInsert(KIO::UDSEntry::UDS_NAME, title);
+        uds.fastInsert(KIO::UDSEntry::UDS_DISPLAY_NAME, title);
+    }
+
+    statEntry(uds);
+    finished();
 }
 
 void FileNameSearchProtocol::listDir(const QUrl &url)
