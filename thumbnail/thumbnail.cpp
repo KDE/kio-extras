1eeaf1a6083b3ff4456ee4281da37eb4b26ac606
Stefan Brüns <stefan.bruens@rwth-aachen.de>
2020-08-21 19:32:30 +0200

    Add mandatory metadata to thumbnail
    According to the spec, at least Thumb::URI and Thumb::MTime are required.
    
    KIO::PreviewJob discards any thumbnails without these and recreates
    the thumbnails.

--- thumbnail/thumbnail.cpp
+++ thumbnail/thumbnail.cpp
@@ -734,6 +734,11 @@ bool ThumbnailProtocol::createSubThumbnail(QImage& thumbnail, const QString& fil
                 // The thumbnail has been created successfully. Store the thumbnail
                 // to the cache for future access.
                 if (thumbnailfile.open(QIODevice::WriteOnly | QIODevice::Truncate)) {
+                    QFileInfo fi(filePath);
+                    thumbnail.setText(QStringLiteral("Thumb::URI"), QString::fromUtf8(fileUrl.toEncoded()));
+                    thumbnail.setText(QStringLiteral("Thumb::MTime"), QString::number(fi.lastModified().toSecsSinceEpoch()));
+                    thumbnail.setText(QStringLiteral("Thumb::Size"), QString::number(fi.size()));
+
                     savedCorrectly = thumbnail.save(&thumbnailfile, "PNG");
                 }
             } else {
