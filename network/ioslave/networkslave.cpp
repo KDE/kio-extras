KDE/kio-extras

commit 730a5936a17ed95d38fe6bbb97311f7ace89363e

Add UDS_ACCESS to UDSEntry for roots of network: recentdocuments and recentlyused:/
This allows KFileItem to mark those folders not writable and disable pasteInto actions.

network/ioslave/networkslave.cpp
@@ -270,7 +270,7 @@ void NetworkSlave::feedEntryAsNetwork( KIO::UDSEntry* entry )
     entry->fastInsert( KIO::UDSEntry::UDS_DISPLAY_NAME, i18n("Network"));
 //     entry->fastInsert( KIO::UDSEntry::UDS_ICON_NAME,    NetworkIconName );
     entry->fastInsert( KIO::UDSEntry::UDS_MIME_TYPE,    QLatin1String(Mimetypes::NetworkMimetype) );
-
+    entry->fastInsert( KIO::UDSEntry::UDS_ACCESS,       S_IRUSR | S_IXUSR );
 }
 
 void NetworkSlave::feedEntryAsDevice( KIO::UDSEntry* entry, const Mollet::NetDevice& deviceData )
@@ -279,9 +279,9 @@ void NetworkSlave::feedEntryAsDevice( KIO::UDSEntry* entry, const Mollet::NetDev
     entry->fastInsert( KIO::UDSEntry::UDS_NAME,         deviceData.hostAddress() );
     entry->fastInsert( KIO::UDSEntry::UDS_DISPLAY_NAME, deviceData.name() );
     entry->fastInsert( KIO::UDSEntry::UDS_FILE_TYPE,    S_IFDIR );
+    entry->fastInsert( KIO::UDSEntry::UDS_ACCESS,       S_IRUSR | S_IXUSR );
 //     entry->fastInsert( KIO::UDSEntry::UDS_ICON_NAME,    NetDevice::iconName(deviceData.type()) );
     entry->fastInsert( KIO::UDSEntry::UDS_MIME_TYPE,    QLatin1String(Mimetypes::DeviceMimetype[deviceData.type()]) );
-
 }
 
 void NetworkSlave::feedEntryAsService( KIO::UDSEntry* entry, const Mollet::NetService& serviceData )

recentdocuments/recentdocuments.cpp
@@ -148,13 +148,14 @@ void RecentDocuments::stat(const QUrl& url)
         //
         QString dirName = i18n("Recent Documents");
         KIO::UDSEntry uds;
-        uds.reserve(6);
+        uds.reserve(7);
         uds.fastInsert(KIO::UDSEntry::UDS_NAME, dirName);
         uds.fastInsert(KIO::UDSEntry::UDS_DISPLAY_NAME, dirName);
         uds.fastInsert(KIO::UDSEntry::UDS_DISPLAY_TYPE, dirName);
         uds.fastInsert(KIO::UDSEntry::UDS_ICON_NAME, QString::fromLatin1("document-open-recent"));
         uds.fastInsert(KIO::UDSEntry::UDS_FILE_TYPE, S_IFDIR);
         uds.fastInsert(KIO::UDSEntry::UDS_MIME_TYPE, QString::fromLatin1("inode/directory"));
+        uds.fastInsert(KIO::UDSEntry::UDS_ACCESS, S_IRUSR | S_IXUSR);
 
         statEntry(uds);
         finished();

recentlyused/recentlyused.cpp
@@ -222,13 +222,14 @@ void RecentlyUsed::listDir(const QUrl &url)
 KIO::UDSEntry RecentlyUsed::udsEntryForRoot(const QString &dirName, const QString &iconName)
 {
     KIO::UDSEntry uds;
-    uds.reserve(6);
+    uds.reserve(7);
     uds.fastInsert(KIO::UDSEntry::UDS_NAME, dirName);
     uds.fastInsert(KIO::UDSEntry::UDS_DISPLAY_NAME, dirName);
     uds.fastInsert(KIO::UDSEntry::UDS_DISPLAY_TYPE, dirName);
     uds.fastInsert(KIO::UDSEntry::UDS_ICON_NAME, iconName);
     uds.fastInsert(KIO::UDSEntry::UDS_FILE_TYPE, S_IFDIR);
     uds.fastInsert(KIO::UDSEntry::UDS_MIME_TYPE, QStringLiteral("inode/directory"));
+    uds.fastInsert(KIO::UDSEntry::UDS_ACCESS, S_IRUSR | S_IXUSR);
     return uds;
 }
 

commit ec9725264eaadb518480f6cf688027e63baf0022

Fix Windows build
Build was broken with
730a5936a17ed95d38fe6bbb97311f7ace89363e

network/ioslave/networkslave.cpp
@@ -35,6 +35,10 @@
 #include <QEventLoop>
 #include <QDebug>
 
+#ifdef Q_OS_WIN
+#include <sys/stat.h>
+#endif
+
 // static const char NetworkIconName[] = "network-workgroup";
 
 
@@ -270,7 +274,11 @@ void NetworkSlave::feedEntryAsNetwork( KIO::UDSEntry* entry )
     entry->fastInsert( KIO::UDSEntry::UDS_DISPLAY_NAME, i18n("Network"));
 //     entry->fastInsert( KIO::UDSEntry::UDS_ICON_NAME,    NetworkIconName );
     entry->fastInsert( KIO::UDSEntry::UDS_MIME_TYPE,    QLatin1String(Mimetypes::NetworkMimetype) );
+#ifdef Q_OS_WIN
+    entry->fastInsert( KIO::UDSEntry::UDS_ACCESS,       _S_IREAD | _S_IWRITE );
+#else
     entry->fastInsert( KIO::UDSEntry::UDS_ACCESS,       S_IRUSR | S_IXUSR );
+#endif
 }
 
 void NetworkSlave::feedEntryAsDevice( KIO::UDSEntry* entry, const Mollet::NetDevice& deviceData )
@@ -279,7 +287,11 @@ void NetworkSlave::feedEntryAsDevice( KIO::UDSEntry* entry, const Mollet::NetDev
     entry->fastInsert( KIO::UDSEntry::UDS_NAME,         deviceData.hostAddress() );
     entry->fastInsert( KIO::UDSEntry::UDS_DISPLAY_NAME, deviceData.name() );
     entry->fastInsert( KIO::UDSEntry::UDS_FILE_TYPE,    S_IFDIR );
+#ifdef Q_OS_WIN
+    entry->fastInsert( KIO::UDSEntry::UDS_ACCESS,       _S_IREAD | _S_IWRITE );
+#else
     entry->fastInsert( KIO::UDSEntry::UDS_ACCESS,       S_IRUSR | S_IXUSR );
+#endif
 //     entry->fastInsert( KIO::UDSEntry::UDS_ICON_NAME,    NetDevice::iconName(deviceData.type()) );
     entry->fastInsert( KIO::UDSEntry::UDS_MIME_TYPE,    QLatin1String(Mimetypes::DeviceMimetype[deviceData.type()]) );
 }

recentdocuments/recentdocuments.cpp
@@ -15,6 +15,10 @@
 
 #include <stdio.h>
 
+#ifdef Q_OS_WIN
+#include <sys/stat.h>
+#endif
+
 extern "C" int Q_DECL_EXPORT kdemain(int argc, char **argv)
 {
     // necessary to use other kio slaves
@@ -155,8 +159,11 @@ void RecentDocuments::stat(const QUrl& url)
         uds.fastInsert(KIO::UDSEntry::UDS_ICON_NAME, QString::fromLatin1("document-open-recent"));
         uds.fastInsert(KIO::UDSEntry::UDS_FILE_TYPE, S_IFDIR);
         uds.fastInsert(KIO::UDSEntry::UDS_MIME_TYPE, QString::fromLatin1("inode/directory"));
-        uds.fastInsert(KIO::UDSEntry::UDS_ACCESS, S_IRUSR | S_IXUSR);
-
+#ifdef Q_OS_WIN
+        uds.fastInsert(KIO::UDSEntry::UDS_ACCESS, _S_IREAD | _S_IWRITE );
+#else
+        uds.fastInsert(KIO::UDSEntry::UDS_ACCESS, S_IRUSR | S_IXUSR );
+#endif
         statEntry(uds);
         finished();
     }

recentlyused/recentlyused.cpp
@@ -33,6 +33,10 @@
 #include <KActivities/Stats/ResultModel>
 #include <KActivities/Stats/Terms>
 
+#ifdef Q_OS_WIN
+#include <sys/stat.h>
+#endif
+
 namespace KAStats = KActivities::Stats;
 
 using namespace KAStats;
@@ -229,7 +233,11 @@ KIO::UDSEntry RecentlyUsed::udsEntryForRoot(const QString &dirName, const QStrin
     uds.fastInsert(KIO::UDSEntry::UDS_ICON_NAME, iconName);
     uds.fastInsert(KIO::UDSEntry::UDS_FILE_TYPE, S_IFDIR);
     uds.fastInsert(KIO::UDSEntry::UDS_MIME_TYPE, QStringLiteral("inode/directory"));
-    uds.fastInsert(KIO::UDSEntry::UDS_ACCESS, S_IRUSR | S_IXUSR);
+#ifdef Q_OS_WIN
+    uds.fastInsert(KIO::UDSEntry::UDS_ACCESS, _S_IREAD | _S_IWRITE );
+#else
+    uds.fastInsert(KIO::UDSEntry::UDS_ACCESS, S_IRUSR | S_IXUSR );
+#endif
     return uds;
 }
 