a5e17084e5ec97add7dea94934e02d0feaa98ad5
Friedrich W. H. Kossebau <kossebau@kde.org>
2021-06-17 15:36:44 +0200

    Drop network: KIO slave
    Never really left the stage of being a prototype for a hardware-centric
    meta-protocol KIO slave, also not just file system centric, and
    its presence is confusing compared to the remote KIO slave, which also
    is tighter integrated with Dolpin & Co.
    
    It had 12 years of chance* to take over, but did not, so time to take it
    back from the playing field.
    
    * https://frinring.wordpress.com/2009/04/20/network-kioslave-entered-kdereview/

--- network/network/builder/abstractnetworkbuilder.cpp
+++
@@ -1,9 +0,0 @@
-/*
-    This file is part of the Mollet network library, part of the KDE project.
-
-    SPDX-FileCopyrightText: 2009 Friedrich W. H. Kossebau <kossebau@kde.org>
-
-    SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
-*/
-
-#include "abstractnetworkbuilder.h"
