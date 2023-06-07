89bd92e76599a95cd772dcd85610d1fe50763409
Luigi Toscano <luigi.toscano@tiscali.it>
2023-06-08 01:25:48 +0200

    doc: kioslave5 -> kioworker6
    Just documentation and its references, the code itself should be ported
    to the new classes.

--- bookmarks/kio_bookmarks_html.cpp
+++ bookmarks/kio_bookmarks_html.cpp
@@ -146,7 +146,7 @@ void BookmarksProtocol::echoHead(const QString &redirect)
     indent++;
     echo("<a title=\"" + i18n("Configuration") + "\" href=\"/config\"><img src=\"/icon/preferences-system?size=32\"/></a>");
     echo("<a title=\"" + i18n("Edit bookmarks") + "\" href=\"/editbookmarks\"><img src=\"/icon/bookmarks-organize?size=32\"/></a>");
-    echo("<a title=\"" + i18n("Help") + "\" href=\"help:/kioslave/bookmarks.html\"><img src=\"/icon/help-contents?size=32\"/></a>");
+    echo("<a title=\"" + i18n("Help") + "\" href=\"help:/kioworker6/bookmarks.html\"><img src=\"/icon/help-contents?size=32\"/></a>");
     indent--;
     echo("</div>");*/
 
