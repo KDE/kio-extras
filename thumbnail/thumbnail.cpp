KDE/kio-extras

commit 69dc61d7dd34bb530c6a0966d490f1f1f6b97f9e

Utilize KIO::PreviewJob::availableThumbnailerPlugins
This method caches the results internally and also takes care of converting legacy KServiceTypeTrader plugins.

thumbnail/thumbnail.cpp
@@ -38,8 +38,6 @@
 #include <KConfigGroup>
 #include <KFileItem>
 #include <KLocalizedString>
-#include <KPluginInfo>
-#include <KServiceTypeTrader>
 #include <KSharedConfig>
 
 #include <kio/previewjob.h>
@@ -334,25 +332,8 @@ void ThumbnailProtocol::get(const QUrl &url)
     finished();
 }
 
-static QVector<KPluginMetaData> availablePlugins()
-{
-    auto jsonMetaDataPlugins = KPluginMetaData::findPlugins(QStringLiteral("kf" QT_STRINGIFY(QT_VERSION_MAJOR) "/thumbcreator"));
-    QT_WARNING_PUSH
-    QT_WARNING_DISABLE_CLANG("-Wdeprecated-declarations")
-    QT_WARNING_DISABLE_GCC("-Wdeprecated-declarations")
-    // TODO KF6 remove this compat codepath
-    const KService::List plugins = KServiceTypeTrader::self()->query(QStringLiteral("ThumbCreator"));
-    for (const auto &plugin : plugins) {
-        if (KPluginInfo info(plugin); info.isValid()) {
-            jsonMetaDataPlugins << info.toMetaData();
-        }
-    }
-    QT_WARNING_POP
-    return jsonMetaDataPlugins;
-}
-
 KPluginMetaData ThumbnailProtocol::pluginForMimeType(const QString& mimeType) {
-    const static QVector<KPluginMetaData> plugins = availablePlugins();
+    const QVector<KPluginMetaData> plugins = KIO::PreviewJob::availableThumbnailerPlugins();
     for (const KPluginMetaData &plugin : plugins) {
         if (plugin.supportsMimeType(mimeType)) {
             return plugin;
@@ -678,16 +659,13 @@ ThumbCreatorWithMetadata* ThumbnailProtocol::getThumbCreator(const QString& plug
         if (plugin.contains(QLatin1String("kf5") + QDir::separator() + QLatin1String("thumbcreator"))) {
             data = KPluginMetaData(plugin);
         } else {
-            QT_WARNING_PUSH
-            QT_WARNING_DISABLE_CLANG("-Wdeprecated-declarations")
-            QT_WARNING_DISABLE_GCC("-Wdeprecated-declarations")
-            // TODO KF6 remove this compat codepath
-            const QString constraint = QStringLiteral("Library == '%1'").arg(QFileInfo(plugin).fileName());
-            const KService::List plugins = KServiceTypeTrader::self()->query(QStringLiteral("ThumbCreator"), constraint);
-            if (!plugins.isEmpty()) {
-                data = KPluginInfo(plugins.first()).toMetaData();
+            const QVector<KPluginMetaData> plugins = KIO::PreviewJob::availableThumbnailerPlugins();
+            auto findPluginIt = std::find_if(plugins.begin(), plugins.end(), [&plugin](const KPluginMetaData &data) {
+                return data.pluginId() == plugin;
+            });
+            if (findPluginIt != plugins.end()) {
+                data = *findPluginIt;
             }
-            QT_WARNING_POP
         }
         if (!data.isValid()) {
             qCWarning(KIO_THUMBNAIL_LOG) << "Plugin not found:" << plugin;

thumbnail/CMakeLists.txt
@@ -66,11 +66,6 @@ ecm_qt_declare_logging_category(kio_thumbnail
     EXPORT KIO_EXTRAS
 )
 
-target_compile_definitions(kio_thumbnail
-    PRIVATE
-        -DKSERVICE_DISABLE_DEPRECATED_BEFORE_AND_AT=0x055100 # KMimeTypeTrader
-)
-
 target_link_libraries(kio_thumbnail
     KF5::CoreAddons
     KF5::KIOCore

commit e68a2f24cdd940a126e5b0e8d14285738d32fa14

[Thumbnail] Fix lookup of legacy thumbnailer plugins
For most legacy plugins, "filename" and plugin id are the same, but
the marble plugin "marblethumbnail" provides e.g. marble_thumbnail_kml.

The KIO previewjob.cpp uses the filename:
`job->addMetaData(QStringLiteral("plugin"), currentItem.plugin.fileName());`

Regression from commit 69dc61d7dd34 ("Utilize
KIO::PreviewJob::availableThumbnailerPlugins").

thumbnail/thumbnail.cpp
@@ -675,7 +675,7 @@ ThumbCreatorWithMetadata* ThumbnailProtocol::getThumbCreator(const QString& plug
         } else {
             const QVector<KPluginMetaData> plugins = KIO::PreviewJob::availableThumbnailerPlugins();
             auto findPluginIt = std::find_if(plugins.begin(), plugins.end(), [&plugin](const KPluginMetaData &data) {
-                return data.pluginId() == plugin;
+                return data.fileName() == plugin;
             });
             if (findPluginIt != plugins.end()) {
                 data = *findPluginIt;