73cb34263e582e5ebef1bbb7bb927f6b1756350f
Mirco Miranda <mirco.miranda@systemceramics.com>
2023-08-28 20:03:23 +0000

    Fix crash with corrupted EXR images
    Fix repeated crashes when opening folders with [corrupted EXR files](https://github.com/AcademySoftwareFoundation/openexr-images/tree/main/Damaged).
    
    ![image](/uploads/4e87bed940145f2cf61834d40f778576/image.png)
    
    (cherry picked from commit e42f170630d11e62a206a1d6db0dbdbc02ebced0)

--- thumbnail/exrcreator.cpp
+++ thumbnail/exrcreator.cpp
@@ -29,44 +29,48 @@ EXRCreator::EXRCreator(QObject *parent, const QVariantList &args)
 
 KIO::ThumbnailResult EXRCreator::create(const KIO::ThumbnailRequest &request)
 {
-    const QByteArray encodedPath = QFile::encodeName(request.url().toLocalFile());
-    Imf::InputFile in (encodedPath.constData());
-    const Imf::Header &h = in.header();
-
-    if ( h.hasPreviewImage() ) {
-        qCDebug(KIO_THUMBNAIL_EXR_LOG) << "EXRcreator - using preview";
-        const Imf::PreviewImage &preview = in.header().previewImage();
-        QImage qpreview(preview.width(), preview.height(), QImage::Format_RGB32);
-        for ( unsigned int y=0; y < preview.height(); y++ ) {
-            for ( unsigned int x=0; x < preview.width(); x++ ) {
-                const Imf::PreviewRgba &q = preview.pixels()[x+(y*preview.width())];
-                qpreview.setPixel( x, y, qRgba(q.r, q.g, q.b, q.a) );
+    try {
+        const QByteArray encodedPath = QFile::encodeName(request.url().toLocalFile());
+        Imf::InputFile in (encodedPath.constData());
+        const Imf::Header &h = in.header();
+        if ( h.hasPreviewImage() ) {
+            qCDebug(KIO_THUMBNAIL_EXR_LOG) << "EXRcreator - using preview";
+            const Imf::PreviewImage &preview = h.previewImage();
+            QImage qpreview(preview.width(), preview.height(), QImage::Format_RGB32);
+            for ( unsigned int y=0; y < preview.height(); y++ ) {
+                for ( unsigned int x=0; x < preview.width(); x++ ) {
+                    const Imf::PreviewRgba &q = preview.pixels()[x+(y*preview.width())];
+                    qpreview.setPixel( x, y, qRgba(q.r, q.g, q.b, q.a) );
+                }
             }
+            return KIO::ThumbnailResult::pass(qpreview);
         }
-        return KIO::ThumbnailResult::pass(qpreview);
-    } else {
-        // do it the hard way
-        // We ignore maximum size when just extracting the thumbnail
-        // from the header, but it is very expensive to render large
-        // EXR images just to turn it into an icon, so we go back
-        // to honoring it in here.
-        qCDebug(KIO_THUMBNAIL_EXR_LOG) << "EXRcreator - using original image";
-        KSharedConfig::Ptr config = KSharedConfig::openConfig();
-        KConfigGroup configGroup( config, "PreviewSettings" );
-        const qint64 maxSize = configGroup.readEntry( "MaximumSize", std::numeric_limits<qint64>::max() );
-        const qint64 fileSize = QFile(request.url().toLocalFile()).size();
-        if ( (fileSize > 0) && (fileSize < maxSize) ) {
-            QImage img;
-            if (!img.load(request.url().toLocalFile())) {
-                return KIO::ThumbnailResult::fail();
-            }
-            if (img.depth() != 32) {
-                img = img.convertToFormat( QImage::Format_RGB32 );
-            }
-            return KIO::ThumbnailResult::pass(img);
-        } else {
+    } catch (const std::exception &e) {
+        qCDebug(KIO_THUMBNAIL_EXR_LOG) << e.what();
+        return KIO::ThumbnailResult::fail();
+    }
+
+    // do it the hard way
+    // We ignore maximum size when just extracting the thumbnail
+    // from the header, but it is very expensive to render large
+    // EXR images just to turn it into an icon, so we go back
+    // to honoring it in here.
+    qCDebug(KIO_THUMBNAIL_EXR_LOG) << "EXRcreator - using original image";
+    KSharedConfig::Ptr config = KSharedConfig::openConfig();
+    KConfigGroup configGroup( config, "PreviewSettings" );
+    const qint64 maxSize = configGroup.readEntry( "MaximumSize", std::numeric_limits<qint64>::max() );
+    const qint64 fileSize = QFile(request.url().toLocalFile()).size();
+    if ( (fileSize > 0) && (fileSize < maxSize) ) {
+        QImage img;
+        if (!img.load(request.url().toLocalFile())) {
             return KIO::ThumbnailResult::fail();
         }
+        if (img.depth() != 32) {
+            img = img.convertToFormat( QImage::Format_RGB32 );
+        }
+        return KIO::ThumbnailResult::pass(img);
+    } else {
+        return KIO::ThumbnailResult::fail();
     }
 }
 
