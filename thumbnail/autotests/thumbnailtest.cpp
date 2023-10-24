KDE/kio-extras

commit 041e311af7f227ffd4c29c916d7239eeb79f637e

thumbnail: better support non-shm case. i.e Windows
This fix testThumbnail for Windows

thumbnail/autotests/thumbnailtest.cpp
@@ -47,7 +47,7 @@ private Q_SLOTS:
         QFETCH(qreal, dpr);
 
         QStandardPaths::setTestModeEnabled(true);
-        qputenv("KIOSLAVE_ENABLE_TESTMODE", "1"); // ensure the worker call QStandardPaths::setTestModeEnabled too
+        qputenv("KIOWORKER_ENABLE_TESTMODE", "1"); // ensure the worker call QStandardPaths::setTestModeEnabled too
 
         // wipe thumbnail cache so we always start clean
         QDir cacheDir(QStandardPaths::writableLocation(QStandardPaths::GenericCacheLocation));
@@ -77,10 +77,10 @@ private Q_SLOTS:
         QSignalSpy gotPreviewSpy(job, &KIO::PreviewJob::gotPreview);
         QSignalSpy resultSpy(job, &KIO::PreviewJob::result);
 
-        gotPreviewSpy.wait();
         resultSpy.wait();
 
-        QVERIFY(failedSpy.empty());
+        QVERIFY2(failedSpy.empty(), qPrintable(job->errorString()));
+        QVERIFY(!gotPreviewSpy.empty());
     }
 };
 

thumbnail/thumbnail.cpp
@@ -293,34 +293,39 @@ KIO::WorkerResult ThumbnailProtocol::get(const QUrl &url)
 
     convertToStandardRgb(img);
 
-    const QString shmid = metaData("shmid");
-    if (shmid.isEmpty()) {
 #ifdef THUMBNAIL_HACK
-        if (direct) {
-            // If thumbnail was called directly from Konqueror, then the image needs to be raw
-            // qDebug() << "RAW IMAGE TO STREAM";
-            QBuffer buf;
-            if (!buf.open(QIODevice::WriteOnly)) {
-                return KIO::WorkerResult::fail(KIO::ERR_INTERNAL, i18n("Could not write image."));
-            }
-            img.save(&buf, "PNG");
-            buf.close();
-            mimeType("image/png");
-            data(buf.buffer());
-        } else
-#endif
-        {
-            QByteArray imgData;
-            QDataStream stream(&imgData, QIODevice::WriteOnly);
-            // qDebug() << "IMAGE TO STREAM";
-            stream << img;
-            mimeType("application/octet-stream");
-            data(imgData);
+    if (direct) {
+        // If thumbnail was called directly from Konqueror, then the image needs to be raw
+        // qDebug() << "RAW IMAGE TO STREAM";
+        QBuffer buf;
+        if (!buf.open(QIODevice::WriteOnly)) {
+            return KIO::WorkerResult::fail(KIO::ERR_INTERNAL, i18n("Could not write image."));
         }
-    } else {
+        img.save(&buf, "PNG");
+        buf.close();
+        mimeType("image/png");
+        data(buf.buffer());
+        return KIO::WorkerResult::pass();
+    }
+#endif
+
+    QByteArray imgData;
+    QDataStream stream(&imgData, QIODevice::WriteOnly);
+
+    // Keep in sync with kio/src/previewjob.cpp
+    const quint8 format = img.format() | 0x80;
+    stream << img.width() << img.height() << format << ((int)img.devicePixelRatio()) << img;
+
 #ifndef Q_OS_WIN
-        QByteArray imgData;
-        QDataStream stream(&imgData, QIODevice::WriteOnly);
+    const QString shmid = metaData("shmid");
+    if (shmid.isEmpty())
+#endif
+    {
+        // qDebug() << "IMAGE TO STREAM";
+        stream << img;
+    }
+#ifndef Q_OS_WIN
+    else {
         // qDebug() << "IMAGE TO SHMID";
         void *shmaddr = shmat(shmid.toInt(), nullptr, 0);
         if (shmaddr == (void *)-1) {
@@ -334,15 +339,13 @@ KIO::WorkerResult ThumbnailProtocol::get(const QUrl &url)
             return KIO::WorkerResult::fail(KIO::ERR_INTERNAL, i18n("Image is too big for the shared memory segment"));
             shmdt((char *)shmaddr);
         }
-        // Keep in sync with kio/src/previewjob.cpp
-        const quint8 format = img.format() | 0x80;
-        stream << img.width() << img.height() << format << ((int)img.devicePixelRatio());
         memcpy(shmaddr, img.bits(), img.sizeInBytes());
         shmdt((char *)shmaddr);
-        mimeType("application/octet-stream");
-        data(imgData);
-#endif
     }
+#endif
+    mimeType("application/octet-stream");
+    data(imgData);
+
     return KIO::WorkerResult::pass();
 }
 

commit 3508c7d46f1d3b10ff827966e65965b97a346f1d

thumbnail: convert indexed color images before sending metadata
Update comments added in 059af081afc872f646c02bf053118e284b658973

Fix regression introduced in 041e311af7f227ffd4c29c916d7239eeb79f637e that changed the format after the metadata was sent.

thumbnail/autotests/thumbnailtest.cpp
@@ -38,6 +38,10 @@ private Q_SLOTS:
         // ignoring embedded thumbnail as it is too small
         QTest::addRow("jpg_embedded_thumbnail2") << "castle.jpg"
                                                  << "castle_thumb_256.png" << 2.0;
+
+        // image using 4 colors table
+        QTest::addRow("Screen_color_test_Amiga_4colors") << "Screen_color_test_Amiga_4colors.png"
+                                                         << "Screen_color_test_Amiga_4colors_converted.png" << 2.0;
     }
 
     void testThumbnail()

thumbnail/exrcreator.cpp
@@ -65,9 +65,6 @@ KIO::ThumbnailResult EXRCreator::create(const KIO::ThumbnailRequest &request)
         if (!img.load(request.url().toLocalFile())) {
             return KIO::ThumbnailResult::fail();
         }
-        if (img.depth() != 32) {
-            img = img.convertToFormat(QImage::Format_RGB32);
-        }
         return KIO::ThumbnailResult::pass(img);
     } else {
         return KIO::ThumbnailResult::fail();

thumbnail/imagecreator.cpp
@@ -90,9 +90,6 @@ KIO::ThumbnailResult ImageCreator::create(const KIO::ThumbnailRequest &request)
 
     QImage img;
     ir.read(&img);
-    if (!img.isNull() && img.depth() != 32) {
-        img = img.convertToFormat(img.hasAlphaChannel() ? QImage::Format_ARGB32 : QImage::Format_RGB32);
-    }
 
     if (!img.isNull()) {
         return KIO::ThumbnailResult::pass(img);

thumbnail/thumbnail.cpp
@@ -270,13 +270,20 @@ KIO::WorkerResult ThumbnailProtocol::get(const QUrl &url)
         }
     }
 
-    scaleDownImage(img, m_width, m_height);
-
     if (img.isNull()) {
         return KIO::WorkerResult::fail(KIO::ERR_INTERNAL, i18n("Failed to create a thumbnail."));
     }
 
+    // image quality and size corrections
+    scaleDownImage(img, m_width, m_height);
+
     convertToStandardRgb(img);
+
+    if (img.colorCount() > 0) {
+        // images using indexed color format, are not loaded properly by QImage ctor using in shm code path
+        // convert the format to regular RGB
+        img = img.convertToFormat(img.hasAlphaChannel() ? QImage::Format_ARGB32 : QImage::Format_RGB32);
+    }
 
     if (direct) {
         // If thumbnail was called directly from Konqueror, then the image needs to be raw
@@ -312,9 +319,6 @@ KIO::WorkerResult ThumbnailProtocol::get(const QUrl &url)
         void *shmaddr = shmat(shmid.toInt(), nullptr, 0);
         if (shmaddr == (void *)-1) {
             return KIO::WorkerResult::fail(KIO::ERR_INTERNAL, i18n("Failed to attach to shared memory segment %1", shmid));
-        }
-        if (img.format() != QImage::Format_ARGB32) { // KIO::PreviewJob and this code below completely ignores colortable :-/,
-            img = img.convertToFormat(QImage::Format_ARGB32); //  so make sure there is none
         }
         struct shmid_ds shmStat;
         if (shmctl(shmid.toInt(), IPC_STAT, &shmStat) == -1 || shmStat.shm_segsz < (uint)img.sizeInBytes()) {