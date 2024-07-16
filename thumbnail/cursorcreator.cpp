ad721c69d38d2da2501dc6d3f80858539a1bbd0f
Akseli Lahtinen <akselmo@akselmo.dev>
2024-07-16 16:17:25 +0300

    Prototype moving cursor creation plugin to a binary

--- thumbnail/cursorcreator.cpp
+++ thumbnail/cursorcreator.cpp
@@ -8,9 +8,15 @@
 
 #include <QFile>
 #include <QImage>
+#include <QMimeData>
+#include <QMimeDatabase>
+#include <thumbnail.h>
 
 #include <KPluginFactory>
 
+#include <QApplication>
+#include <QCommandLineParser>
+#include <QtWidgets/qapplication.h>
 #include <X11/Xcursor/Xcursor.h>
 #include <X11/Xlib.h>
 
@@ -38,6 +44,41 @@ KIO::ThumbnailResult CursorCreator::create(const KIO::ThumbnailRequest &request)
     }
 
     return KIO::ThumbnailResult::fail();
+}
+
+int CursorCreator::run(const KIO::ThumbnailRequest &request, const QString &outputLocation)
+{
+    auto c = create(request);
+    if (c.isValid()) {
+        c.image().save(outputLocation);
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    QApplication application{argc, argv};
+    application.setApplicationName(u"cursorcreator"_qs);
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription(u"Thumbnailer binary file"_qs);
+    parser.addHelpOption();
+    parser.addOptions({
+        {{u"i"_qs, u"input"_qs}, u"The input file"_qs, u"input"_qs},
+        {{u"o"_qs, u"output"_qs}, u"The output file"_qs, u"output"_qs},
+        {{u"s"_qs, u"size"_qs}, u"The width in pixels"_qs, u"size"_qs},
+    });
+    parser.process(application);
+    int size = parser.value(u"size"_qs).toInt();
+    auto filepath = QUrl::fromLocalFile(parser.value(u"input"_qs));
+    QMimeDatabase db;
+    QMimeType type = db.mimeTypeForFile(filepath.toLocalFile());
+    auto req = KIO::ThumbnailRequest(filepath, QSize(size, size), type.name(), 1.0, 1.0);
+    parser.process(application);
+    CursorCreator creator(nullptr, QVariantList());
+    return creator.run(req, parser.value(u"output"_qs));
 }
 
 #include "cursorcreator.moc"
