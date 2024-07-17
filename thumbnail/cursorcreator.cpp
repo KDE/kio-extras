cce0faa66104a77bab7e4a5b733d6d7c31ef55b3
Akseli Lahtinen <akselmo@akselmo.dev>
2024-07-17 10:09:50 +0300

    Add more error handling

--- thumbnail/cursorcreator.cpp
+++ thumbnail/cursorcreator.cpp
@@ -16,7 +16,6 @@
 
 #include <QApplication>
 #include <QCommandLineParser>
-#include <QtWidgets/qapplication.h>
 #include <X11/Xcursor/Xcursor.h>
 #include <X11/Xlib.h>
 
@@ -60,18 +59,26 @@ int main(int argc, char **argv)
         {{u"s"_qs, u"size"_qs}, u"The width in pixels"_qs, u"size"_qs},
     });
     parser.process(application);
+
+    auto input = QUrl::fromLocalFile(parser.value(u"input"_qs));
+    auto output = QUrl::fromLocalFile(parser.value(u"output"_qs));
     int size = parser.value(u"size"_qs).toInt();
-    auto filepath = QUrl::fromLocalFile(parser.value(u"input"_qs));
-    QMimeDatabase db;
-    QMimeType type = db.mimeTypeForFile(filepath.toLocalFile());
-    auto req = KIO::ThumbnailRequest(filepath, QSize(size, size), type.name(), 1.0, 1.0);
     parser.process(application);
+    if (!input.isValid() || !output.isValid() || size <= 0) {
+        qWarning() << "Failed to parse parameters!";
+        return 1;
+    }
+
+    QMimeDatabase db;
+    QMimeType type = db.mimeTypeForFile(input.toLocalFile());
+    auto req = KIO::ThumbnailRequest(input, QSize(size, size), type.name(), 1.0, 1.0);
     CursorCreator creator(nullptr, QVariantList());
     auto c = creator.create(req);
     if (c.isValid()) {
-        c.image().save(parser.value(u"output"_qs));
+        c.image().save(output.toLocalFile());
         return 0;
     } else {
+        qWarning() << "Failed to generate a thumbnail!";
         return 1;
     }
 }
