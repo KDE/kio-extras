KDE/kio-extras

commit 26133784f67cb5bdd0a56ee74737a4f4f5344388

thumbnails: Use direct lookup for plugins
KIO is gaining support for thumbnail support using binaries listed in
/usr/share/thumbnailers .

For compatibility availablePlugins() returns metadata for these binaries as though
they were KDE plugins.

This works for applications to filter, but it means we should use
the direct path for actual plugin lookup in the thumbnailing code.

thumbnail/thumbnail.cpp
@@ -321,7 +321,7 @@ KIO::WorkerResult ThumbnailProtocol::get(const QUrl &url)
 
 KPluginMetaData ThumbnailProtocol::pluginForMimeType(const QString &mimeType)
 {
-    const QList<KPluginMetaData> plugins = KIO::PreviewJob::availableThumbnailerPlugins();
+    const QList<KPluginMetaData> plugins = KPluginMetaData::findPlugins(QStringLiteral("kf6/thumbcreator"));
     for (const KPluginMetaData &plugin : plugins) {
         if (plugin.supportsMimeType(mimeType)) {
             return plugin;

commit df0f384e4d81e879e7627b526be8cc00c9189695

thumbnail: Fetch the list of thumbnailer plugins only once
The list doesn't meaningfully change during the application lifetime

Fixes a performance regression from 26133784f67cb5bdd0a56ee74737a4f4f5344388

thumbnail/thumbnail.cpp
@@ -321,7 +321,7 @@ KIO::WorkerResult ThumbnailProtocol::get(const QUrl &url)
 
 KPluginMetaData ThumbnailProtocol::pluginForMimeType(const QString &mimeType)
 {
-    const QList<KPluginMetaData> plugins = KPluginMetaData::findPlugins(QStringLiteral("kf6/thumbcreator"));
+    static const QList<KPluginMetaData> plugins = KPluginMetaData::findPlugins(QStringLiteral("kf6/thumbcreator"));
     for (const KPluginMetaData &plugin : plugins) {
         if (plugin.supportsMimeType(mimeType)) {
             return plugin;