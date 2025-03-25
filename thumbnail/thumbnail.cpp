KDE/kio-extras

commit 1fe1548a7e595107d5ed00b30b031b74ac658ed4

Use offscreen platform plugin for thumbnailers
Some thumbnailer plugins can cause spurious windows to appear (see referenced bugreport for an example).
This can be disruptive when viewing a directory with many html files in Dolphin.
The proposed solution is to use offscreen platform abstraction plugin for all thumbnailers.

BUG: 500173

thumbnail/thumbnail.cpp
@@ -103,6 +103,12 @@ extern "C" Q_DECL_EXPORT int kdemain(int argc, char **argv)
     // need QGuiApplication
     qunsetenv("SESSION_MANAGER");
 
+    // Some plugins may cause unwanted windows to appear
+    // (e.g. Webarchiver, see https://bugs.kde.org/show_bug.cgi?id=500173).
+    // This will not let any plugin to create auxilliary windows on the screen
+    // while generating thumbnails.
+    qputenv("QT_QPA_PLATFORM", "offscreen");
+
     // Some thumbnail plugins use QWidget classes for the rendering,
     // so use QApplication here, not just QGuiApplication
     QApplication app(argc, argv);

commit e11dfd86bd075afc7ff5c5eec70bc5d2377c4168

Extend using offscreen platform for thumbnail creation by using original platform theme.
A previous commit 1fe1548a introduced offscreen platform to the thumbnailer but also a regression where directory thumbnails were built using the default platform theme. This results in e.g. incorrect colors being used.

To preserve the original platform theme we create an auxiliary QApplication and then set the platform theme with QT_QPA_PLATFORMTHEME environment variable.

BUG: 500173

thumbnail/CMakeLists.txt
@@ -84,10 +84,13 @@ ecm_qt_declare_logging_category(kio_thumbnail
 )
 
 target_link_libraries(kio_thumbnail
-    KF6::CoreAddons
-    KF6::KIOCore
-    KF6::KIOWidgets
-    KF6::I18n
+    PUBLIC
+        KF6::CoreAddons
+        KF6::KIOCore
+        KF6::KIOWidgets
+        KF6::I18n
+    PRIVATE
+        Qt6::GuiPrivate
 )
 
 install(TARGETS kio_thumbnail DESTINATION ${KDE_INSTALL_PLUGINDIR}/kf6/kio)

thumbnail/thumbnail.cpp
@@ -53,6 +53,9 @@
 
 #include "imagefilter.h"
 
+#include <private/qguiapplication_p.h> // QGuiApplicationPrivate::platformTheme()
+#include <qpa/qplatformtheme.h>
+
 // Recognized metadata entries:
 // mimeType     - the mime type of the file, used for the overlay icon if any
 // width        - maximum width for the thumbnail
@@ -107,7 +110,17 @@ extern "C" Q_DECL_EXPORT int kdemain(int argc, char **argv)
     // (e.g. Webarchiver, see https://bugs.kde.org/show_bug.cgi?id=500173).
     // This will not let any plugin to create auxilliary windows on the screen
     // while generating thumbnails.
+    // Retrieve theme name from the default platform...
+    QString themeName;
+    {
+        QGuiApplication aux(argc, argv);
+        if (auto platformTheme = QGuiApplicationPrivate::platformTheme()) {
+            themeName = platformTheme->name();
+        }
+    }
+    // ...and force using offscreen platform with the default platform's theme
     qputenv("QT_QPA_PLATFORM", "offscreen");
+    qputenv("QT_QPA_PLATFORMTHEME", themeName.toUtf8());
 
     // Some thumbnail plugins use QWidget classes for the rendering,
     // so use QApplication here, not just QGuiApplication