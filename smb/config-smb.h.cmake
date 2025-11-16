33f5ab2d1a42233d7924d78738e16a42bd1e91d5
Andreas Schneider <asn@cryptomilk.org>
2025-11-16 11:16:24 +0100

    cmake: Check if libsmbclient has POSIX extension support

--- smb/config-smb.h.cmake
+++ smb/config-smb.h.cmake
@@ -1,3 +1,5 @@
 /* Define to 1 if you have the <utime.h> header file. */
 #cmakedefine HAVE_UTIME_H 1
 #cmakedefine HAVE_READDIRPLUS2 1
+#cmakedefine HAVE_SMBC_FGETXATTR 1
+#cmakedefine HAVE_SMBC_SETOPTIONPOSIXEXTENSIONS 1
